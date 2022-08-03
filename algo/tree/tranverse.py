5c1e2fd120383f6118d6003b3b6759ad34604104
shiyang07ca <shiyang07ca@gmail.com>
2022-08-03 21:41:04 +0800

    x

--- algo/tree/tranverse.py
+++ algo/tree/tranverse.py
@@ -18,6 +18,7 @@ r"""
 
 """
 
+
 class Node:
     # value = None
 
@@ -30,44 +31,118 @@ class Node:
 import unittest
 
 
-def pre_order_recur(root):
+def pre_order_recur(root, ans=[]):
     if root is None:
         return
 
-    print(root.value + " ")
-    pre_order_recur(root.left)
-    pre_order_recur(root.right)
+    # print(root.value + " ")
+    ans.append(root.value)
+    pre_order_recur(root.left, ans)
+    pre_order_recur(root.right, ans)
+
+    return ans
 
 
-def in_order_recur(root):
+def in_order_recur(root, ans=[]):
     if root is None:
         return
 
-    in_order_recur(root.left)
-    print(root.value + " ")
-    in_order_recur(root.right)
+    in_order_recur(root.left, ans)
+    # print(root.value + " ")
+    ans.append(root.value)
+    in_order_recur(root.right, ans)
+
+    return ans
 
 
-
-def pos_order_recur(root):
+def pos_order_recur(root, ans=[]):
     if root is None:
         return
 
-    pos_order_recur(root.left)
-    pos_order_recur(root.right)
-    print(root.value + " ")
+    pos_order_recur(root.left, ans)
+    pos_order_recur(root.right, ans)
+    # print(root.value + " ")
+    ans.append(root.value)
+
+    return ans
 
 
 def pre_order_iter(root):
-    pass
+    if root is None:
+        return
+
+    stack = [root]
+    ans = []
+    while stack:
+        cur = stack.pop()
+        ans.append(cur.value)
+        if cur.right:
+            stack.append(cur.right)
+        if cur.left:
+            stack.append(cur.left)
+
+    return ans
 
 
 def in_order_iter(root):
-    pass
+    if root is None:
+        return
+
+    stack = []
+    ans = []
+    while root or stack:
+        if root:
+            stack.append(root)
+            root = root.left
+        else:
+            node = stack.pop()
+            ans.append(node.value)
+            root = node.right
+
+    return ans
 
 
-def pos_order_iter(root):
-    pass
+def pos_order_iter1(root):
+    if root is None:
+        return
+
+    stack1, stack2 = [root], []
+
+    while stack1:
+        cur = stack1.pop()
+        stack2.append(cur)
+        if cur.left:
+            stack1.append(cur.left)
+        if cur.right:
+            stack1.append(cur.right)
+
+    ans = []
+    while stack2:
+        ans.append(stack2.pop().value)
+
+    return ans
+
+
+def pos_order_iter2(root):
+    if root is None:
+        return
+
+    ans = []
+    h = root
+    c = None
+    stack = [h]
+    while stack:
+        c = stack[-1]
+        if c.left and h != c.left and h != c.right:
+            stack.append(c.left)
+        elif c.right and h != c.right:
+            stack.append(c.right)
+        else:
+            ans.append(stack.pop().value)
+            h = c
+
+    return ans
+
 
 r"""
          a
@@ -78,16 +153,16 @@ r"""
 
 """
 
+
 class TestRecur(unittest.TestCase):
     def test(self):
-        print('aaa')
-        a = Node('a')
-        b = Node('b')
-        c = Node('c')
-        d = Node('d')
-        e = Node('e')
-        f = Node('f')
-        g = Node('g')
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        f = Node("f")
+        g = Node("g")
 
         b.left = d
         b.right = e
@@ -98,34 +173,31 @@ class TestRecur(unittest.TestCase):
         a.left = b
         a.right = c
 
+        self.assertEqual(
+            ["a", "b", "d", "e", "c", "f", "g"],
+            pre_order_recur(a),
+        )
 
-        pre_order_recur(a)
+        self.assertEqual(
+            ["d", "b", "e", "a", "f", "c", "g"],
+            in_order_recur(a),
+        )
 
-        print('================')
-
-        in_order_recur(a)
-
-        print('================')
-
-        pos_order_recur(a)
-
-
-        # self.assertEqual(
-        #     [1, 1, 2, 3, 5, 8, 13, 21],
-        #     fib(8),
-        # )
+        self.assertEqual(
+            ["d", "e", "b", "f", "g", "c", "a"],
+            pos_order_recur(a),
+        )
 
 
 class TestIter(unittest.TestCase):
     def test(self):
-        print('aaa')
-        a = Node('a')
-        b = Node('b')
-        c = Node('c')
-        d = Node('d')
-        e = Node('e')
-        f = Node('f')
-        g = Node('g')
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        f = Node("f")
+        g = Node("g")
 
         b.left = d
         b.right = e
@@ -136,6 +208,26 @@ class TestIter(unittest.TestCase):
         a.left = b
         a.right = c
 
+        self.assertEqual(
+            ["a", "b", "d", "e", "c", "f", "g"],
+            pre_order_iter(a),
+        )
 
-if __name__ == '__main__':
+        self.assertEqual(
+            ["d", "b", "e", "a", "f", "c", "g"],
+            in_order_iter(a),
+        )
+
+        self.assertEqual(
+            ["d", "e", "b", "f", "g", "c", "a"],
+            pos_order_iter1(a),
+        )
+
+        self.assertEqual(
+            ["d", "e", "b", "f", "g", "c", "a"],
+            pos_order_iter2(a),
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
