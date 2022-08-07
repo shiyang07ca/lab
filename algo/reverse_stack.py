39fa379da0cd8db86681979714dfc159d2c4ff9e
shiyang07ca <shiyang07ca@gmail.com>
2022-08-07 23:24:07 +0800

    x

---
+++ algo/reverse_stack.py
@@ -0,0 +1,48 @@
+"""
+
+给你一个栈，请你逆序这个栈，不能申请额外的数据结构，
+只能使用递归函数，如何实现？
+
+"""
+
+import unittest
+
+
+from typing import List
+
+
+class Solution:
+    def f(self, stack):
+        result = stack.pop()
+        if not stack:
+            return result
+        else:
+            last = self.f(stack)
+            stack.append(result)
+            return last
+
+    def reverse(self, stack):
+        if not stack:
+            return
+
+        last = self.f(stack)
+        self.reverse(stack)
+        stack.append(last)
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        nums = [1, 5, 2]
+        self.sl.reverse(nums)
+        self.assertEqual(tuple(nums), (2, 5, 1))
+
+        nums = [1, 5, 233, 7]
+        self.sl.reverse(nums)
+        self.assertEqual(tuple(nums), (7, 233, 5, 1))
+
+
+if __name__ == "__main__":
+    unittest.main()
