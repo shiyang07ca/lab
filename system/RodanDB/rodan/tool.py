a1616ca28c4c12aeb3ff6fe0e6c47996e698e10e
shiyang07ca <shiyang07ca@gmail.com>
2022-08-09 14:02:20 +0800

    db repl

--- system/RodanDB/rodan/tool.py
+++ system/RodanDB/rodan/tool.py
@@ -5,8 +5,25 @@ import sys
 # BAD_VERB = 2
 # BAD_KEY = 3
 
-EXIT_SUCCESS = 0
-BAD_INPUT = -1
+from enum import Enum, unique, auto
+
+EXIT_SUCCESS = 1
+BAD_INPUT = 2
+
+
+@unique
+class MetaCommandResult(Enum):
+
+    META_COMMAND_SUCCESS = auto()
+    META_COMMAND_UNRECOGNIZED_COMMAND = auto()
+
+
+@unique
+class PrepareResult(Enum):
+
+    PREPARE_SUCCESS = auto()
+    PREPARE_UNRECOGNIZED_STATEMENT = auto()
+
 
 # def usage():
 #     print("Usage:", file=sys.stderr)
@@ -25,23 +42,49 @@ def read_input():
 
     if len(line) <= 0:
         print("Error reading input")
-        exit(BAD_INPUT)
+        exit(BAD_INPUT.value)
 
     return line
 
 
-def main(argv):
+def do_meta_cmd(line):
+    if line == '.exit':
+        # return MetaCommandResult.META_COMMAND_SUCCESS
+        print('exit.')
+        import ipdb
+        ipdb.set_trace()
+        exit(EXIT_SUCCESS)
 
+    return MetaCommandResult.META_COMMAND_UNRECOGNIZED_COMMAND
+
+
+def prepare_statement(line):
+    if 'insert' in line:
+        return PrepareResult.PREPARE_SUCCESS
+    elif 'select' in line:
+        return PrepareResult.PREPARE_SUCCESS
+
+    return PrepareResult.PREPARE_UNRECOGNIZED_STATEMENT
+
+
+def exec_statement(statement):
+    print('Executed.')
+
+
+def main(argv):
     print('================ RodanDB ================')
     # REPL
     while True:
         print_prompt()
         line = read_input()
 
-        if line == '.exit':
-            exit(EXIT_SUCCESS)
-        else:
-            print(f"Unrecognized command {line}")
+        if line[0] == '.':
+            ans = do_meta_cmd(line)
+            if ans == MetaCommandResult.META_COMMAND_SUCCESS:
+                continue
+            elif ans == MetaCommandResult.META_COMMAND_UNRECOGNIZED_COMMAND:
+                print(f"Unrecognized command '{line}'")
+                continue
 
 
 if __name__ == "__main__":
