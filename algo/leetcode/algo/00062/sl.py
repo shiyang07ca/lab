866ac401fe433b026f814493efbeb33488c076ed
shiyang07ca <shiyang07ca@gmail.com>
2022-09-16 22:17:01 +0800

    lc 62

---
+++ algo/leetcode/algo/00062/sl.py
@@ -0,0 +1,94 @@
+"""
+
+62. 不同路径
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+
+问总共有多少条不同的路径？
+
+
+
+示例 1：
+
+
+输入：m = 3, n = 7
+输出：28
+
+
+示例 2：
+
+输入：m = 3, n = 2
+输出：3
+解释：
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向下 -> 向下
+2. 向下 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向下
+
+示例 3：
+
+输入：m = 7, n = 3
+输出：28
+
+示例 4：
+
+输入：m = 3, n = 3
+输出：6
+
+
+提示：
+
+1 <= m, n <= 100
+题目数据保证答案小于等于 2 * 109
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+from os.path import abspath, join, dirname
+from typing import *
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+
+class Solution:
+    from functools import cache
+
+    @cache
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+
+        if m > 1 and n > 1:
+            return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        elif m > 1:
+            return self.uniquePaths(m - 1, n) + 1
+        elif n > 1:
+            return self.uniquePaths(m, n - 1) + 1
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
