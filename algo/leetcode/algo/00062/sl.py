18626a413f0fd8d8df0eaf3e0eece02c19dd3370
shiyang07ca <shiyang07ca@gmail.com>
2022-09-17 11:36:21 +0800

    lc 62

--- algo/leetcode/algo/00062/sl.py
+++ algo/leetcode/algo/00062/sl.py
@@ -63,6 +63,60 @@ sys.path.insert(0, parentdir)
 from algo.tree.builder import *
 
 
+"""
+作者：powcai
+链接：https://leetcode.cn/problems/unique-paths/solution/dong-tai-gui-hua-by-powcai-2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+思路一：排列组合
+
+因为机器到底右下角，向下几步，向右几步都是固定的，
+
+比如，m=3, n=2，我们只要向下 1 步，向右 2 步就一定能到达终点。
+
+所以有 C (m+n-2, m-1)
+
+
+
+思路二：动态规划
+
+我们令 dp[i][j] 是到达 i, j 最多路径
+
+动态方程：dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+注意，对于第一行 dp[0][j]，或者第一列 dp[i][0]，由于都是在边界，所以只能为 1
+
+时间复杂度：O(m*n)O(m∗n)
+
+空间复杂度：O(m * n)O(m∗n)
+
+优化：因为我们每次只需要 dp[i-1][j],dp[i][j-1]
+
+"""
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return int(
+            math.factorial(m + n - 2) / math.factorial(m - 1) / math.factorial(n - 1)
+        )
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * n] + [[1] + [0] * (n - 1) for _ in range(m - 1)]
+        # print(dp)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
+
+
+################################################################
+
+
 class Solution:
     from functools import cache
 
