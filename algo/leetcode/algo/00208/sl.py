d6e588df093023c2c4bc9d11b9b15daf0e783fd6
shiyang07ca <shiyang07ca@gmail.com>
2022-09-21 22:25:28 +0800

    lc 208

---
+++ algo/leetcode/algo/00208/sl.py
@@ -0,0 +1,160 @@
+"""
+
+[208] Implement Trie (Prefix Tree)
+
+
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+
+	Trie() Initializes the trie object.
+	void insert(String word) Inserts the string word into the trie.
+	boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+	boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+
+
+--------------------------------------------------
+
+Example 1:
+
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+
+
+
+Constraints:
+
+
+	1 <= word.length, prefix.length <= 2000
+	word and prefix consist only of lowercase English letters.
+	At most 3 * 10⁴ calls in total will be made to insert, search, and startsWith.
+
+
+################################################################
+
+
+208. 实现 Trie (前缀树)
+
+Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+
+请你实现 Trie 类：
+
+Trie() 初始化前缀树对象。
+void insert(String word) 向前缀树中插入字符串 word 。
+boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+
+
+示例：
+
+输入
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+
+输出
+[null, null, true, false, true, null, true]
+
+解释
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // 返回 True
+trie.search("app");     // 返回 False
+trie.startsWith("app"); // 返回 True
+trie.insert("app");
+trie.search("app");     // 返回 True
+
+
+提示：
+
+1 <= word.length, prefix.length <= 2000
+word 和 prefix 仅由小写英文字母组成
+insert、search 和 startsWith 调用次数 总计 不超过 3 * 104 次
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+from os.path import abspath, join, dirname
+from typing import *
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+
+class Trie:
+    def __init__(self):
+        self.is_leaf = False
+        self.next = dict()
+
+    def insert(self, word: str) -> None:
+        cur = self
+        for c in word:
+            if c not in cur.next:
+                cur.next[c] = Trie()
+            cur = cur.next[c]
+        cur.is_leaf = True
+
+    def search(self, word: str) -> bool:
+        cur = self
+        for c in word:
+            if c not in cur.next:
+                return False
+            cur = cur.next[c]
+
+        return cur.is_leaf
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self
+        for c in prefix:
+            if c not in cur.next:
+                return False
+            cur = cur.next[c]
+
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
