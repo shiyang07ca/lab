738313ed75f836977466d2c2bf4868c40c494cc0
shiyang07ca <shiyang07ca@gmail.com>
2022-09-22 23:27:24 +0800

    lc 337

---
+++ algo/leetcode/algo/00337/sl.py
@@ -0,0 +1,146 @@
+"""
+
+[337] House Robber III
+
+
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
+
+Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+
+
+--------------------------------------------------
+
+Example 1:
+
+
+Input: root = [3,2,3,null,3,null,1]
+Output: 7
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+
+
+--------------------------------------------------
+
+Example 2:
+
+
+Input: root = [3,4,5,1,3,null,1]
+Output: 9
+Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+
+
+Constraints:
+
+
+	The number of nodes in the tree is in the range [1, 10⁴].
+	0 <= Node.val <= 10⁴
+
+
+################################################################
+
+337. 打家劫舍 III
+
+小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+
+除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+
+给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+
+
+
+示例 1:
+
+            3
+        2        3
+         3         1
+
+输入: root = [3,2,3,null,3,null,1]
+输出: 7
+解释: 小偷一晚能够盗取的最高金额 3 + 3 + 1 = 7
+
+示例 2:
+             3
+         4       5
+       1  3        1
+
+
+输入: root = [3,4,5,1,3,null,1]
+输出: 9
+解释: 小偷一晚能够盗取的最高金额 4 + 5 = 9
+
+
+提示：
+
+树的节点数在 [1, 104] 范围内
+0 <= Node.val <= 104
+
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+from os.path import abspath, join, dirname
+from typing import *
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+
+class Solution:
+    from functools import cache
+
+    @cache
+    def rob(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        if root.left is None and root.right is None:
+            return root.val
+        elif root.left is None:
+            return max(
+                root.val + self.rob(root.right.left) + self.rob(root.right.right),
+                self.rob(root.right),
+            )
+        elif root.right is None:
+            return max(
+                root.val + self.rob(root.left.left) + self.rob(root.left.right),
+                self.rob(root.left),
+            )
+        l = self.rob(root.left)
+        r = self.rob(root.right)
+
+        return max(
+            l + r,
+            root.val
+            + self.rob(root.left.left)
+            + self.rob(root.left.right)
+            + self.rob(root.right.left)
+            + self.rob(root.right.right),
+        )
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
