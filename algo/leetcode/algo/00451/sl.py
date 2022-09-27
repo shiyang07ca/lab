6973fd75c9167a0a5db38b1d5d38c6a8affc6c1d
shiyang07ca <shiyang07ca@gmail.com>
2022-09-27 23:46:16 +0800

    lc 451

---
+++ algo/leetcode/algo/00451/sl.py
@@ -0,0 +1,101 @@
+"""
+
+[451] Sort Characters By Frequency
+
+
+Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+
+--------------------------------------------------
+
+Example 1:
+
+
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+
+--------------------------------------------------
+
+Example 2:
+
+
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+
+
+--------------------------------------------------
+
+Example 3:
+
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+
+
+
+Constraints:
+
+
+	1 <= s.length <= 5 * 10⁵
+	s consists of uppercase and lowercase English letters and digits.
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+from os.path import abspath, join, dirname
+from typing import *
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        c_count = defaultdict(int)
+        for c in s:
+            c_count[c] += 1
+
+        pair_list = [(v, c) for c, v in c_count.items()]
+        pair_list.sort(reverse=True)
+
+        c_list = []
+        for n, c in pair_list:
+            for _ in range(n):
+                c_list.append(c)
+
+        return "".join(c_list)
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
