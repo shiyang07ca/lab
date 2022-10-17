61f07d8c9babdf65ab5dc8b13d5832641190680b
iceyasha <iceyasha@gmail.com>
2022-10-17 05:57:33 +0000

    x

--- pl/py/tmodule.py
+++ pl/py/tmodule.py
@@ -1,7 +1,17 @@
-class Auxiliary:
-    def do_somethin(self):
-        pass
+import logging
 
+# create logger
+module_logger = logging.getLogger('spam_application.auxiliary')
+
+class Auxiliary:
+    def __init__(self):
+        self.logger = logging.getLogger('spam_application.auxiliary.Auxiliary')
+        self.logger.info('creating an instance of Auxiliary')
+
+    def do_something(self):
+        self.logger.info('doing something')
+        a = 1 + 1
+        self.logger.info('done doing something')
 
 def some_function():
-    return None
+    module_logger.info('received a call to "some_function"')
