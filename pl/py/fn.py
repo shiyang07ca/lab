2ee55d96d3b22d8311722c2062f0ed0f868a0035
iceyasha <iceyasha@gmail.com>
2022-10-18 11:46:45 +0000

    x

---
+++ pl/py/fn.py
@@ -0,0 +1,13 @@
+from functools import partial
+
+# A normal function
+def f(a, b, c, x):
+    return a + b + c + x
+
+
+# A partial function that calls f with
+# a as 3, b as 1 and c as 4.
+g = partial(f, 3, 1, 4)
+
+# Calling g()
+print(g(5))
