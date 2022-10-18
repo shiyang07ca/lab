6d5be2609079c60f6cc1153a978cea8994c92ea0
iceyasha <iceyasha@gmail.com>
2022-10-18 05:42:25 +0000

    ds dsu

---
+++ algo/data_structure/dsu/dsu.py
@@ -0,0 +1,28 @@
+"""
+
+并查集
+Disjoint Set Union or DSU. Often it is also called Union Find
+
+
+
+
+"""
+
+
+class DSU:
+    pass
+
+
+import unittest
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.dsu = DSU()
+
+    def test_1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
