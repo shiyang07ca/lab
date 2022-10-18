7c37cfb128592b90c5c2c541273aa34d1a74ff3f
iceyasha <iceyasha@gmail.com>
2022-10-18 06:01:42 +0000

    ds dsu

--- algo/data_structure/dsu/dsu.py
+++ algo/data_structure/dsu/dsu.py
@@ -1,5 +1,8 @@
 """
 
+# https://oi-wiki.org/ds/dsu/
+# https://cp-algorithms.com/data_structures/disjoint_set_union.html
+
 并查集
 Disjoint Set Union or DSU. Often it is also called Union Find
 
@@ -10,7 +13,17 @@ Disjoint Set Union or DSU. Often it is also called Union Find
 
 
 class DSU:
-    pass
+    def __init__(self):
+        self.pa = {}
+
+    def find(self, x):
+        if self.pa[x] != x:
+            # 路径压缩
+            self.pa[x] = self.find(self.pa[x])
+        return self.pa[x]
+
+    def union(self, a, b):
+        self.pa[self.find(a)] = self.find(b)
 
 
 import unittest
@@ -18,10 +31,14 @@ import unittest
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
-        self.dsu = DSU()
+        """"""
 
-    def test_1(self):
-        pass
+    def test1(self):
+        d1 = DSU()
+        d2 = DSU()
+
+        d1.union(1, 1)
+        d2.union(1, 1)
 
 
 if __name__ == "__main__":
