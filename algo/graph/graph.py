e58c3ce4eee0354d092741bcad90da8e29904b3a
iceyasha <iceyasha@gmail.com>
2022-10-25 15:49:53 +0000

    temp

--- algo/graph/graph.py
+++ algo/graph/graph.py
@@ -3,11 +3,19 @@
 """
 
 """
+
 """
 
 from collections import deque
 
 """
+BFS
+
+https://cp-algorithms.com/graph/breadth-first-search.html
+
+应用
+- 在一个无权图上求从起点到其他所有点的最短路径。
+
 --------------------------------------------------------------------------------
     Breadth First Search.
         Args :  G - Dictionary of edges
--- algo/oj/leetcode/templates/tmpl.py
+++ algo/oj/leetcode/templates/tmpl.py
@@ -3,18 +3,23 @@ import inspect
 import os
 import unittest
 from os.path import abspath, join, dirname
+from itertools import *
+from collections import deque
+
 from typing import *
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 parentdir = os.path.dirname(parentdir)  # algo
 parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # oj
 parentdir = os.path.dirname(parentdir)  # algo
 sys.path.insert(0, parentdir)
 # print(sys.path)
 
 
 from algo.tree.builder import *
+
 
 
 class TestSolution(unittest.TestCase):
