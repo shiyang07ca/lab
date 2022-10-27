60bf118a389b56fdf4f0064764dc5beb82155516
iceyasha <iceyasha@gmail.com>
2022-10-27 05:50:41 +0000

    x

--- algo/ds/monotone_queue.py
+++ algo/ds/monotone_queue.py
@@ -1,4 +1,29 @@
-r"""
+"""
+"""
+"""
+"""
+
+
+"""
+
+单调队列 Monotone Queue
+
+需要不断维护队列的单调性，时刻保证队列元素从大到小或从小到大
+前置知识：双指针
+以固定窗口大小的区间最大值为例（此时维护的是一个从大到小的单调队列）：
+每次向右移动一格左指针，在移动前，如果左指针指向的元素在队首左侧，说明左指针指向的元素小于队首，移动左指针不会改变区间最大值，直接移动左指针即可；
+如果左指针指向的就是队首，那么移动左指针会使区间最大值变小（变为单调队列队首之后的那个元素），我们要弹出队首。
+这样无论是何种情况，都保证了在移动左指针后，单调队列的队首始终为当前区间的最大值。
+https://oi-wiki.org/ds/monotonous-queue/
+https://oi-wiki.org/dp/opt/monotonous-queue-stack/
+https://cp-algorithms.com/data_structures/stack_queue_modification.html
+https://blog.csdn.net/weixin_43914593/article/details/105791217 算法竞赛专题解析（13）：DP优化(3)--单调队列优化
+
+"""
+
+
+"""
+应用
 
 
 生成窗口最大值数组
@@ -23,9 +48,6 @@ r"""
 输入：整型数组arr，窗口大小为w。
 
 输出：一个长度为n-w+1的数组res，res[i]表示每一种窗口状态下的最大值。以本题为例，结果应该返回{5，5，5，4，6，7}。
-
-
-
 """
 
 
@@ -59,7 +81,7 @@ import unittest
 
 
 class Test(unittest.TestCase):
-    def test(self):
+    def test_get_max_window(self):
         print("aaa")
         self.assertEqual(
             [5, 5, 5, 4, 6, 7], get_max_window([4, 3, 5, 4, 3, 3, 6, 7], 3)
