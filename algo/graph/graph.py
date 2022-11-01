f98007afbff322ce781e65eae6f593b42937ac5f
iceyasha <iceyasha@gmail.com>
2022-11-01 14:56:04 +0000

    graph

--- algo/graph/graph.py
+++ algo/graph/graph.py
@@ -151,7 +151,8 @@ class GraphLFStar:
             vi, vj, val = edges[i]
             self.add_edge(i, vi, vj, val)
 
-    # 向图的边集数组中添加边：vi - vj，权值为 val
+            # 向图的边集数组中添加边：vi - vj，权值为 val
+
     def add_edge(self, index, vi, vj, val):
         if not self.__valid(vi) or not self.__valid(vj):
             raise ValueError(str(vi) + " or " + str(vj) + " is not a valid vertex.")
@@ -188,13 +189,54 @@ class GraphLFStar:
                 index = self.edges[index].next  # 取顶点 vi 相连的下一条边在边集数组的下标
 
 
-print("GraphLFStar")
-graph = GraphLFStar(7, 7)
-edges = [[1, 2, 5], [1, 5, 6], [2, 4, 7], [4, 3, 9], [3, 1, 2], [5, 6, 8], [6, 4, 3]]
-graph.creatGraph(edges)
-print(graph.get_edge(4, 3))
-print(graph.get_edge(4, 5))
-graph.printGraph()
+# 哈希表实现邻接表
+class VertexNodeHash:  # 顶点信息类
+    def __init__(self, vi):
+        self.vi = vi  # 顶点
+        self.adj_edges = dict()  # 顶点的邻接边
+
+
+class GraphHashAdjList:
+    def __init__(self):
+        self.vertices = dict()  # 顶点
+
+    # 图的创建操作，edges 为边信息
+    def creatGraph(self, edges=[]):
+        for vi, vj, val in edges:
+            self.add_edge(vi, vj, val)
+
+    # 向图中添加节点
+    def add_vertex(self, vi):
+        vertex = VertexNodeHash(vi)
+        self.vertices[vi] = vertex
+
+    # 向图的邻接表中添加边：vi - vj，权值为 val
+    def add_edge(self, vi, vj, val):
+        if vi not in self.vertices:
+            self.add_vertex(vi)
+        if vj not in self.vertices:
+            self.add_vertex(vj)
+
+        self.vertices[vi].adj_edges[vj] = val
+
+    # 获取 vi - vj 边的权值
+    def get_edge(self, vi, vj):
+        if vi in self.vertices and vj in self.vertices[vi].adj_edges:
+            return self.vertices[vi].adj_edges[vj]
+        return None
+
+    # 根据邻接表打印图的边
+    def printGraph(self):
+        for vi in self.vertices:
+            for vj in self.vertices[vi].adj_edges:
+                print(
+                    str(vi)
+                    + " - "
+                    + str(vj)
+                    + " : "
+                    + str(self.vertices[vi].adj_edges[vj])
+                )
+
 
 import unittest
 
@@ -234,6 +276,39 @@ class TestGraph(unittest.TestCase):
         self.adj_list_g.creatGraph(edges)
         print(self.adj_list_g.get_edge(3, 4))
         self.adj_list_g.printGraph()
+
+    def testLFStar(self):
+        print("GraphLFStar")
+        graph = GraphLFStar(7, 7)
+        edges = [
+            [1, 2, 5],
+            [1, 5, 6],
+            [2, 4, 7],
+            [4, 3, 9],
+            [3, 1, 2],
+            [5, 6, 8],
+            [6, 4, 3],
+        ]
+        graph.creatGraph(edges)
+        print(graph.get_edge(4, 3))
+        print(graph.get_edge(4, 5))
+        graph.printGraph()
+
+    def testHashAdjList(self):
+        print("HashAdjList")
+        graph = GraphHashAdjList()
+        edges = [
+            [1, 2, 5],
+            [1, 5, 6],
+            [2, 4, 7],
+            [4, 3, 9],
+            [3, 1, 2],
+            [5, 6, 8],
+            [6, 4, 3],
+        ]
+        graph.creatGraph(edges)
+        print(graph.get_edge(3, 4))
+        graph.printGraph()
 
 
 from collections import deque
