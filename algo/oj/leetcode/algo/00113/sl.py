9812f3eb53196ebf127f6f76c3e1d7d95dad3c6f
iceyasha <iceyasha@gmail.com>
2022-11-12 07:12:19 +0000

    lc 113

---
+++ algo/oj/leetcode/algo/00113/sl.py
@@ -0,0 +1,188 @@
+"""
+
+[113] Path Sum II
+
+
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+
+
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+
+
+Example 3:
+
+
+Input: root = [1,2], targetSum = 0
+Output: []
+
+
+
+Constraints:
+
+
+	The number of nodes in the tree is in the range [0, 5000].
+	-1000 <= Node.val <= 1000
+	-1000 <= targetSum <= 1000
+
+################################################################
+
+
+133. 克隆图
+给你无向 连通 图中一个节点的引用，请你返回该图的 深拷贝（克隆）。
+
+图中的每个节点都包含它的值 val（int） 和其邻居的列表（list[Node]）。
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+
+
+测试用例格式：
+
+简单起见，每个节点的值都和它的索引相同。例如，第一个节点值为 1（val = 1），第二个节点值为 2（val = 2），以此类推。该图在测试用例中使用邻接列表表示。
+
+邻接列表 是用于表示有限图的无序列表的集合。每个列表都描述了图中节点的邻居集。
+
+给定节点将始终是图中的第一个节点（值为 1）。你必须将 给定节点的拷贝 作为对克隆图的引用返回。
+
+
+
+示例 1：
+
+
+
+输入：adjList = [[2,4],[1,3],[2,4],[1,3]]
+输出：[[2,4],[1,3],[2,4],[1,3]]
+解释：
+图中有 4 个节点。
+节点 1 的值是 1，它有两个邻居：节点 2 和 4 。
+节点 2 的值是 2，它有两个邻居：节点 1 和 3 。
+节点 3 的值是 3，它有两个邻居：节点 2 和 4 。
+节点 4 的值是 4，它有两个邻居：节点 1 和 3 。
+
+
+
+示例 2：
+
+输入：adjList = [[]]
+输出：[[]]
+解释：输入包含一个空列表。该图仅仅只有一个值为 1 的节点，它没有任何邻居。
+
+
+示例 3：
+
+输入：adjList = []
+输出：[]
+解释：这个图是空的，它不含任何节点。
+
+
+示例 4：
+
+输入：adjList = [[2],[1]]
+输出：[[2],[1]]
+
+
+提示：
+
+节点数不超过 100 。
+每个节点值 Node.val 都是唯一的，1 <= Node.val <= 100。
+无向图是一个简单图，这意味着图中没有重复的边，也没有自环。
+由于图是无向的，如果节点 p 是节点 q 的邻居，那么节点 q 也必须是节点 p 的邻居。
+图是连通图，你可以从给定节点访问到所有节点。
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+from os.path import abspath, join, dirname
+from itertools import *
+from collections import *
+from copy import *
+from typing import *
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # oj
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+"""
+
+https://algo.itcharge.cn/08.Graph/02.Graph-Traversal/01.Graph-DFS/
+
+思路 1：深度优先搜索
+- 使用哈希表 visitedDict 来存储原图中被访问过的节点和克隆图中对应节点，键值对为 原图被访问过的节点：克隆图中对应节点。
+- 从给定节点开始，以深度优先搜索的方式遍历原图。
+- 如果当前节点被访问过，则返回隆图中对应节点。
+- 如果当前节点没有被访问过，则创建一个新的节点，并保存在哈希表中。
+- 遍历当前节点的邻接节点列表，递归调用当前节点的邻接节点，并将其放入克隆图中对应节点。
+递归结束，返回克隆节点。
+
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+
+        used = {}
+        def dfs(node):
+            if node in used:
+                return used[node]
+
+            new = Node(node.val)
+            used[node] = new
+            for n in node.neighbors:
+                new.neighbors.append(dfs(n))
+            return new
+
+        return dfs(node)
+
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
