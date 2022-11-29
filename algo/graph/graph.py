a35da581c260be4a08775091fa3e19c28fdca843
shiyang07ca <shiyang07ca@gmail.com>
2022-11-29 23:14:33 +0800

    x

--- algo/graph/graph.py
+++ algo/graph/graph.py
@@ -356,6 +356,24 @@ LC743.网络延迟时间 https://leetcode-cn.com/problems/network-delay-time/
 
 """
 
+# Dijkstra 算法模板
+# 返回从 start 到每个点的最短路
+def dijkstra(g: List[List[Tuple[int]]], start: int) -> List[int]:
+  dist = [inf] * len(g)
+  dist[start] = 0
+  h = [(0, start)]
+  while h:
+    d, x = heappop(h)
+    if d > dist[x]:
+      continue
+    for y, wt in g[x]:
+        new_d = dist[x] + wt
+        if new_d < dist[y]:
+            dist[y] = new_d
+            heappush(h, (new_d, y))
+  return dist
+
+
 
 if __name__ == "__main__":
     unittest.main()
