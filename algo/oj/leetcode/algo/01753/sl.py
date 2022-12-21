ec61613f163e50306119120133d9f0fa89b721e6
shiyang07ca <shiyang07ca@gmail.com>
2022-12-21 07:30:29 +0000

    x

--- algo/oj/leetcode/algo/01753/sl.py
+++ algo/oj/leetcode/algo/01753/sl.py
@@ -80,29 +80,74 @@ from heapq import *
 from algo.tree.builder import *
 
 
+"""
+基于贪心思想拿石子，每次从剩余数目前两名的堆中拿，
+每次拿的数目应该等于 第二堆 - 最小堆的数目
+（这样可以使得第二堆和最小堆的石子数量一样）
+"""
+
+
 class Solution:
     def maximumScore(self, a: int, b: int, c: int) -> int:
-        arr = [-a, -b, -c]
-        heapify(arr)
+        h = [-a, -b, -c]
+        heapify(h)
         ans = 0
         while True:
-
-            """
-            基于贪心思想拿石子，每次从剩余数目前两名的堆中拿，
-            每次拿的数目应该等于 第二堆-最小堆的数目（这样可以使得第二堆和最小堆的石子数量一样）
-            """
-            max_ = -heappop(arr)
-            medium = -heappop(arr)
-            min_ = -arr[0]
-            if medium == 0:
+            ma = -heappop(h)
+            med = -heappop(h)
+            mi = -h[0]
+            if med == 0:
                 return ans
 
-            tmp = max(medium - min_, 1)
-            max_ -= tmp
-            medium -= tmp
-            heappush(arr, -max_)
-            heappush(arr, -medium)
-            ans += tmp
+            t = max(med - mi, 1)
+            ans += t
+            heappush(h, -(ma - t))
+            heappush(h, -(med - t))
+
+
+"""
+作者：lcbin
+链接：https://leetcode.cn/problems/maximum-score-from-removing-stones/solution/by-lcbin-01wp/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+每次贪心地从最大的两堆石子中取石头，直到至少有两堆石子为空。
+时间复杂度 O(n)，其中 n 为石子总数。
+"""
+
+
+class Solution:
+    def maximumScore(self, a: int, b: int, c: int) -> int:
+        s = sorted([a, b, c])
+        ans = 0
+        while s[1]:
+            ans += 1
+            s[1] -= 1
+            s[2] -= 1
+            s.sort()
+        return ans
+
+
+"""
+我们不妨设 a ≤ b ≤ c，那么：
+
+- 当 a + b ≤ c 时，我们可以先从 a, c 两堆中取石头，得到分数 a；再从 b, c 两堆中
+取石头，得到分数 b，总分数为 a + b；
+
+- 当 a + b >c 时，这时我们每次会从 c 以及 a 和 b 中较大的那一堆中取石头，最终将
+c 取空。此时 a 和 b 的大小差最多为 1。我们再从 a, b 两堆中取石头，直到不能取为止，
+总分数为 floor((a + b + c) / 2)
+
+时间复杂度 O(1)。
+"""
+
+
+class Solution:
+    def maximumScore(self, a: int, b: int, c: int) -> int:
+        a, b, c = sorted([a, b, c])
+        if a + b < c:
+            return a + b
+        return (a + b + c) >> 1
 
 
 class TestSolution(unittest.TestCase):
