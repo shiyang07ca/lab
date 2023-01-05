3a339fa4345051f8629ac192c1a7a17e35bb2b11
shiyang07ca <shiyang07ca@gmail.com>
2023-01-05 05:13:59 +0000

    x

---
+++ algo/oj/leetcode/algo/01803/sl.py
@@ -0,0 +1,133 @@
+"""
+
+[1803] Count Pairs With XOR in a Range
+
+
+Given a (0-indexed) integer array nums and two integers low and high, return the number of nice pairs.
+
+A nice pair is a pair (i, j) where 0 &lt;= i &lt; j &lt; nums.length and low &lt;= (nums[i] XOR nums[j]) &lt;= high.
+
+
+Example 1:
+
+Input: nums = [1,4,2,7], low = 2, high = 6
+Output: 6
+Explanation: All nice pairs (i, j) are as follows:
+    - (0, 1): nums[0] XOR nums[1] = 5
+    - (0, 2): nums[0] XOR nums[2] = 3
+    - (0, 3): nums[0] XOR nums[3] = 6
+    - (1, 2): nums[1] XOR nums[2] = 6
+    - (1, 3): nums[1] XOR nums[3] = 3
+    - (2, 3): nums[2] XOR nums[3] = 5
+
+
+Example 2:
+
+Input: nums = [9,8,4,2,1], low = 5, high = 14
+Output: 8
+Explanation: All nice pairs (i, j) are as follows:
+​​​​​    - (0, 2): nums[0] XOR nums[2] = 13
+    - (0, 3): nums[0] XOR nums[3] = 11
+    - (0, 4): nums[0] XOR nums[4] = 8
+    - (1, 2): nums[1] XOR nums[2] = 12
+    - (1, 3): nums[1] XOR nums[3] = 10
+    - (1, 4): nums[1] XOR nums[4] = 9
+    - (2, 3): nums[2] XOR nums[3] = 6
+    - (2, 4): nums[2] XOR nums[4] = 5
+
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+1 <= nums[i] <= 2 * 104
+1 <= low <= high <= 2 * 104
+
+
+
+################################################################
+
+# TODO
+
+1803. 统计异或值在范围内的数对有多少
+
+给你一个整数数组 nums （下标 从 0 开始 计数）以及两个整数：low 和 high ，请返回 漂亮数对 的数目。
+
+漂亮数对 是一个形如 (i, j) 的数对，其中 0 <= i < j < nums.length 且 low <= (nums[i] XOR nums[j]) <= high 。
+
+
+示例 1：
+
+输入：nums = [1,4,2,7], low = 2, high = 6
+输出：6
+解释：所有漂亮数对 (i, j) 列出如下：
+    - (0, 1): nums[0] XOR nums[1] = 5
+    - (0, 2): nums[0] XOR nums[2] = 3
+    - (0, 3): nums[0] XOR nums[3] = 6
+    - (1, 2): nums[1] XOR nums[2] = 6
+    - (1, 3): nums[1] XOR nums[3] = 3
+    - (2, 3): nums[2] XOR nums[3] = 5
+
+
+示例 2：
+
+输入：nums = [9,8,4,2,1], low = 5, high = 14
+输出：8
+解释：所有漂亮数对 (i, j) 列出如下：
+​​​​​    - (0, 2): nums[0] XOR nums[2] = 13
+    - (0, 3): nums[0] XOR nums[3] = 11
+    - (0, 4): nums[0] XOR nums[4] = 8
+    - (1, 2): nums[1] XOR nums[2] = 12
+    - (1, 3): nums[1] XOR nums[3] = 10
+    - (1, 4): nums[1] XOR nums[4] = 9
+    - (2, 3): nums[2] XOR nums[3] = 6
+    - (2, 4): nums[2] XOR nums[4] = 5
+
+
+提示：
+
+1 <= nums.length <= 2 * 104
+1 <= nums[i] <= 2 * 104
+1 <= low <= high <= 2 * 104
+
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+
+from itertools import *
+from collections import *
+from copy import *
+from typing import *
+from math import *
+
+from os.path import abspath, join, dirname
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # oj
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
