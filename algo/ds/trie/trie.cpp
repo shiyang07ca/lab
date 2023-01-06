d81001ccd4642150a519900781e3dd2c18a189d4
shiyang07ca <shiyang07ca@gmail.com>
2023-01-06 15:25:45 +0000

    x

---
+++ algo/ds/trie/trie.cpp
@@ -0,0 +1,35 @@
+// 作者：yxc
+// 链接：https://www.acwing.com/blog/content/404/
+// 来源：AcWing
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+int son[N][26], cnt[N], idx;
+// 0号点既是根节点，又是空节点
+// son[][]存储树中每个节点的子节点
+// cnt[]存储以每个节点结尾的单词数量
+
+// 插入一个字符串
+void insert(char *str)
+{
+    int p = 0;
+    for (int i = 0; str[i]; i ++ )
+    {
+        int u = str[i] - 'a';
+        if (!son[p][u]) son[p][u] = ++ idx;
+        p = son[p][u];
+    }
+    cnt[p] ++ ;
+}
+
+// 查询字符串出现的次数
+int query(char *str)
+{
+    int p = 0;
+    for (int i = 0; str[i]; i ++ )
+    {
+        int u = str[i] - 'a';
+        if (!son[p][u]) return 0;
+        p = son[p][u];
+    }
+    return cnt[p];
+}
