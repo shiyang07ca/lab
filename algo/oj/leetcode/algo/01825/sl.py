d125ba7626b300928c048fd7e3d7fd3e340eae45
shiyang07ca <shiyang07ca@gmail.com>
2023-01-19 10:36:34 +0800

    lc 1825

--- algo/oj/leetcode/algo/01825/sl.py
+++ algo/oj/leetcode/algo/01825/sl.py
@@ -21,7 +21,7 @@ Implement the MKAverage class:
 	int calculateMKAverage() Calculates and returns the MKAverage for the current stream rounded down to the nearest integer.
 
 
- 
+
 Example 1:
 
 
@@ -32,7 +32,7 @@ Output
 [null, null, null, -1, null, 3, null, null, null, 5]
 
 Explanation
-MKAverage obj = new MKAverage(3, 1); 
+MKAverage obj = new MKAverage(3, 1);
 obj.addElement(3);        // current elements are [3]
 obj.addElement(1);        // current elements are [3,1]
 obj.calculateMKAverage(); // return -1, because m = 3 and only 2 elements exist.
@@ -48,7 +48,7 @@ obj.calculateMKAverage(); // The last 3 elements are [5,5,5].
                           // The average of [5] equals 5/1 = 5, return 5
 
 
- 
+
 Constraints:
 
 
@@ -83,6 +83,75 @@ sys.path.insert(0, parentdir)
 
 
 from algo.tree.builder import *
+
+from sortedcontainers import SortedList
+
+
+class MKAverage:
+    def __init__(self, m: int, k: int):
+        self.m = m
+        self.k = k
+        self.sum2 = 0
+        self.s1, self.s2, self.s3 = SortedList(), SortedList(), SortedList()
+        self.q = deque()
+
+    def addElement(self, num: int) -> None:
+        self.q.append(num)
+        if len(self.q) <= self.m:
+            self.s2.add(num)
+            self.sum2 += num
+            if len(self.q) == self.m:
+                while len(self.s1) < self.k:
+                    s2mi = self.s2.pop(0)
+                    self.sum2 -= s2mi
+                    self.s1.add(s2mi)
+                while len(self.s3) < self.k:
+                    s2ma = self.s2.pop(-1)
+                    self.sum2 -= s2ma
+                    self.s3.add(s2ma)
+            return
+
+        if len(self.q) == self.m + 1:
+            if num < self.s1[-1]:
+                self.s1.add(num)
+                s1ma = self.s1.pop(-1)
+                self.s2.add(s1ma)
+                self.sum2 += s1ma
+            elif num > self.s3[0]:
+                self.s3.add(num)
+                s3mi = self.s3.pop(0)
+                self.s2.add(s3mi)
+                self.sum2 += s3mi
+            else:
+                self.s2.add(num)
+                self.sum2 += num
+
+            x = self.q.popleft()
+            if x in self.s2:
+                self.s2.remove(x)
+                self.sum2 -= x
+            elif x in self.s1:
+                self.s1.remove(x)
+                s2mi = self.s2.pop(0)
+                self.sum2 -= s2mi
+                self.s1.add(s2mi)
+            elif x in self.s3:
+                self.s3.remove(x)
+                s2ma = self.s2.pop(-1)
+                self.sum2 -= s2ma
+                self.s3.add(s2ma)
+
+    def calculateMKAverage(self) -> int:
+        if len(self.q) < self.m:
+            return -1
+        else:
+            return self.sum2 // (self.m - 2 * self.k)
+
+
+# Your MKAverage object will be instantiated and called as such:
+# obj = MKAverage(m, k)
+# obj.addElement(num)
+# param_2 = obj.calculateMKAverage()
 
 
 class TestSolution(unittest.TestCase):
