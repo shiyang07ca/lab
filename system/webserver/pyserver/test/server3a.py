d51703541a739d8410c7d402b8132feed507deda
shiyang07ca <shiyang07ca@gmail.com>
2023-02-14 14:17:27 +0800

    server

--- system/webserver/pyserver/test/server3a.py
+++ system/webserver/pyserver/test/server3a.py
@@ -5,7 +5,7 @@
 #####################################################################
 import socket
 
-SERVER_ADDRESS = (HOST, PORT) = '', 8888
+SERVER_ADDRESS = (HOST, PORT) = "", 8888
 REQUEST_QUEUE_SIZE = 5
 
 
@@ -25,12 +25,13 @@ def serve_forever():
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listen_socket.bind(SERVER_ADDRESS)
     listen_socket.listen(REQUEST_QUEUE_SIZE)
-    print('Serving HTTP on port {port} ...'.format(port=PORT))
+    print("Serving HTTP on port {port} ...".format(port=PORT))
 
     while True:
         client_connection, client_address = listen_socket.accept()
         handle_request(client_connection)
         client_connection.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     serve_forever()
--- system/webserver/pyserver/test/wsgiapp.py
+++ system/webserver/pyserver/test/wsgiapp.py
@@ -3,7 +3,7 @@ def app(environ, start_response):
 
     This is a starting point for your own Web framework :)
     """
-    status = '200 OK'
-    response_headers = [('Content-Type', 'text/plain')]
+    status = "200 OK"
+    response_headers = [("Content-Type", "text/plain")]
     start_response(status, response_headers)
-    return [b'Hello world from a simple WSGI application!\n']
+    return [b"Hello world from a simple WSGI application!\n"]
