c566e8ec95e145c76979bcbedc57ecc7f2051240
shiyang07ca <shiyang07ca@gmail.com>
2023-02-17 23:48:29 +0800

    lc 3

---
+++ algo/oj/leetcode/algo/00003/sl.py
@@ -0,0 +1,100 @@
+"""
+
+[3] Longest Substring Without Repeating Characters
+
+
+Given a string s, find the length of the longest substring without repeating characters.
+
+
+Example 1:
+
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+
+Example 2:
+
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+
+Example 3:
+
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+
+Constraints:
+
+
+	0 <= s.length <= 5 * 10⁴
+	s consists of English letters, digits, symbols and spaces.
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+
+from itertools import *
+from collections import *
+from copy import *
+from typing import *
+from math import *
+
+from os.path import abspath, join, dirname
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # oj
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        N = len(s)
+        if N <= 1:
+            return N
+
+        cnt = Counter()
+        l = 0
+        ans = 0
+        for r, c in enumerate(s):
+            cnt[c] += 1
+            while cnt[c] > 1 and l <= r:
+                cnt[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+
+        return ans
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
