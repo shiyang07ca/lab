afa552186ba0dc144993274ca1745a11b557e0c5
shiyang07ca <shiyang07ca@gmail.com>
2023-02-26 21:35:35 +0800

    lc 45

---
+++ algo/oj/leetcode/algo/00045/sl.py
@@ -0,0 +1,135 @@
+"""
+
+[45] Jump Game II
+
+
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+
+	0 <= j <= nums[i] and
+	i + j < n
+
+
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+
+Example 1:
+
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+
+Example 2:
+
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+
+
+
+Constraints:
+
+
+	1 <= nums.length <= 10⁴
+	0 <= nums[i] <= 1000
+	It's guaranteed that you can reach nums[n - 1].
+
+################################################################
+
+# TODO
+# tag: greedy
+
+45. 跳跃游戏 II
+
+给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
+
+每个元素 nums[i] 表示从索引 i 向前跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处:
+
+0 <= j <= nums[i]
+i + j < n
+返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
+
+
+
+示例 1:
+
+输入: nums = [2,3,1,1,4]
+输出: 2
+解释: 跳到最后一个位置的最小跳跃数是 2。
+     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+
+
+示例 2:
+
+输入: nums = [2,3,0,1,4]
+输出: 2
+
+
+提示:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000
+题目保证可以到达 nums[n-1]
+
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+
+from itertools import *
+from collections import *
+from copy import *
+from typing import *
+from math import *
+
+from os.path import abspath, join, dirname
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # oj
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+# https://leetcode.cn/problems/jump-game-ii/solution/tiao-yue-you-xi-ii-by-leetcode-solution/
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        right_most = 0
+        ans = 0
+        end = 0
+        for i in range(len(nums) - 1):
+            right_most = max(right_most, i + nums[i])
+            if i == end:
+                end = right_most
+                ans += 1
+
+        return ans
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
