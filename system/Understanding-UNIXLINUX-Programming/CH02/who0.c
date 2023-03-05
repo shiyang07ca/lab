bf786753f71d51582843a0fcc0f869bcb81c810f
shiyang07ca <shiyang07ca@gmail.com>
2023-03-05 15:30:05 +0000

    x

--- system/Understanding-UNIXLINUX-Programming/CH02/who0.c
+++ system/Understanding-UNIXLINUX-Programming/CH02/who0.c
@@ -1,28 +1,27 @@
-#include	<stdio.h>
-#include	<fcntl.h>
-#include	<utmp.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <utmp.h>
 
 /*
  *	who version 0
  *		main outline but no substance
  */
 
-int
-main()
-{
-        int         fd;                 /* for file des of utmp */
-        struct utmp current_record;     /* hold info from file */
-        int         reclen = sizeof(struct utmp);
+int main() {
+  int fd;                     /* for file des of utmp */
+  struct utmp current_record; /* hold info from file */
+  int reclen = sizeof(struct utmp);
 
-        fd = open(UTMP_FILE, O_RDONLY);
-        if ( fd == -1 ){
-                perror( "who0" );       /* report a system error */
-                exit(1);                /* and get out now       */
-        }
+  fd = open(UTMP_FILE, O_RDONLY);
+  if (fd == -1) {
+    perror("who0"); /* report a system error */
+    exit(1);        /* and get out now       */
+  }
 
-        while ( read(fd, &current_record, reclen) == reclen )
-                show_info( &current_record );
+  while (read(fd, &current_record, reclen) == reclen)
+    show_info(&current_record);
 
-        close (fd);
-        return 0;
+  close(fd);
+  return 0;
 }
--- system/Understanding-UNIXLINUX-Programming/CH02/who1.c
+++ system/Understanding-UNIXLINUX-Programming/CH02/who1.c
@@ -1,44 +1,42 @@
 /* who1.c  - a first version of the who program
  *              open, read UTMP file, and show results
  */
-#include	<stdio.h>
-#include	<utmp.h>
-#include	<fcntl.h>
-#include	<unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <utmp.h>
 
-#define	SHOWHOST	/* include remote machine on output */
+#define SHOWHOST /* include remote machine on output */
 
-int main()
-{
-	struct utmp	current_record;	/* read info into here       */
-	int		utmpfd;		/* read from this descriptor */
-	int		reclen = sizeof(current_record);
+int main() {
+  struct utmp current_record; /* read info into here       */
+  int utmpfd;                 /* read from this descriptor */
+  int reclen = sizeof(current_record);
 
-	if ( (utmpfd = open(UTMP_FILE, O_RDONLY)) == -1 ){
-		perror( UTMP_FILE );	/* UTMP_FILE is in utmp.h    */
-		exit(1);
-	}
+  if ((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1) {
+    perror(UTMP_FILE); /* UTMP_FILE is in utmp.h    */
+    exit(1);
+  }
 
-	while ( read(utmpfd, &current_record, reclen) == reclen )
-		show_info(&current_record);
-	close(utmpfd);
-	return 0;			/* went ok */
+  while (read(utmpfd, &current_record, reclen) == reclen)
+    show_info(&current_record);
+  close(utmpfd);
+  return 0; /* went ok */
 }
 /*
  *  show info()
  *	displays contents of the utmp struct in human readable form
  *	*note* these sizes should not be hardwired
  */
-show_info( struct utmp *utbufp )
-{
-	printf("%-8.8s", utbufp->ut_name);	/* the logname	*/
-	printf(" ");				/* a space	*/
-	printf("%-8.8s", utbufp->ut_line);	/* the tty	*/
-	printf(" ");				/* a space	*/
-	printf("%10ld", utbufp->ut_time);	/* login time	*/
-	printf(" ");				/* a space	*/
-#ifdef	SHOWHOST
-	printf("(%s)", utbufp->ut_host);	/* the host	*/
+show_info(struct utmp *utbufp) {
+  printf("%-8.8s", utbufp->ut_name); /* the logname	*/
+  printf(" ");                       /* a space	*/
+  printf("%-8.8s", utbufp->ut_line); /* the tty	*/
+  printf(" ");                       /* a space	*/
+  printf("%10ld", utbufp->ut_time);  /* login time	*/
+  printf(" ");                       /* a space	*/
+#ifdef SHOWHOST
+  printf("(%s)", utbufp->ut_host); /* the host	*/
 #endif
-	printf("\n");				/* newline	*/
+  printf("\n"); /* newline	*/
 }
