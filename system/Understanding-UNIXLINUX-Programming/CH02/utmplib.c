860bf94d3e2ed56ba986de4980f04b4b7a6997bc
shiyang07ca <shiyang07ca@gmail.com>
2023-03-06 22:58:16 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH02/utmplib.c
+++ system/Understanding-UNIXLINUX-Programming/CH02/utmplib.c
@@ -1,4 +1,4 @@
-/* utmplib.c  - functions to buffer reads from utmp file 
+/* utmplib.c  - functions to buffer reads from utmp file
  *
  *      functions are
  *              utmp_open( filename )   - open file
@@ -9,39 +9,37 @@
  *
  *      reads NRECS per read and then doles them out from the buffer
  */
-#include        <stdio.h>
-#include        <fcntl.h>
-#include        <sys/types.h>
-#include        <utmp.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <utmp.h>
 
-#define NRECS   16
-#define NULLUT  ((struct utmp *)NULL)
-#define UTSIZE  (sizeof(struct utmp))
+#define NRECS 16
+#define NULLUT ((struct utmp *)NULL)
+#define UTSIZE (sizeof(struct utmp))
 
-static  char    utmpbuf[NRECS * UTSIZE];                /* storage      */
-static  int     num_recs;                               /* num stored   */
-static  int     cur_rec;                                /* next to go   */
-static  int     fd_utmp = -1;                           /* read from    */
+static char utmpbuf[NRECS * UTSIZE]; /* storage      */
+static int num_recs;                 /* num stored   */
+static int cur_rec;                  /* next to go   */
+static int fd_utmp = -1;             /* read from    */
 
-utmp_open( char *filename )
-{
-        fd_utmp = open( filename, O_RDONLY );           /* open it      */
-        cur_rec = num_recs = 0;                         /* no recs yet  */
-        return fd_utmp;                                 /* report       */
+utmp_open(char *filename) {
+  fd_utmp = open(filename, O_RDONLY); /* open it      */
+  cur_rec = num_recs = 0;             /* no recs yet  */
+  return fd_utmp;                     /* report       */
 }
 
-struct utmp *utmp_next()
-{
-        struct utmp *recp;
+struct utmp *utmp_next() {
+  struct utmp *recp;
 
-        if ( fd_utmp == -1 )                            /* error ?      */
-                return NULLUT;
-        if ( cur_rec==num_recs && utmp_reload()==0 )    /* any more ?   */
-                return NULLUT;
-                                        /* get address of next record    */
-        recp = ( struct utmp *) &utmpbuf[cur_rec * UTSIZE];
-        cur_rec++;
-        return recp;
+  if (fd_utmp == -1) /* error ?      */
+    return NULLUT;
+  if (cur_rec == num_recs && utmp_reload() == 0) /* any more ?   */
+    return NULLUT;
+  /* get address of next record    */
+  recp = (struct utmp *)&utmpbuf[cur_rec * UTSIZE];
+  cur_rec++;
+  return recp;
 }
 
 int utmp_reload()
@@ -49,20 +47,19 @@ int utmp_reload()
  *      read next bunch of records into buffer
  */
 {
-        int     amt_read;
+  int amt_read;
 
-                                                /* read them in         */
-        amt_read = read( fd_utmp , utmpbuf, NRECS * UTSIZE );   
+  /* read them in         */
+  amt_read = read(fd_utmp, utmpbuf, NRECS * UTSIZE);
 
-                                                /* how many did we get? */
-        num_recs = amt_read/UTSIZE;
-                                                /* reset pointer        */
-        cur_rec  = 0;
-        return num_recs;
+  /* how many did we get? */
+  num_recs = amt_read / UTSIZE;
+  /* reset pointer        */
+  cur_rec = 0;
+  return num_recs;
 }
 
-utmp_close()
-{
-        if ( fd_utmp != -1 )                    /* don't close if not   */
-                close( fd_utmp );               /* open                 */
+utmp_close() {
+  if (fd_utmp != -1) /* don't close if not   */
+    close(fd_utmp);  /* open                 */
 }
