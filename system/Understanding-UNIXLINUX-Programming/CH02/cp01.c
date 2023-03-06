e6e0b0f23f65cdcb85b17d0187dc164504e949c6
shiyang07ca <shiyang07ca@gmail.com>
2023-03-06 14:41:52 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH02/cp01.c
+++ system/Understanding-UNIXLINUX-Programming/CH02/cp01.c
@@ -3,50 +3,47 @@
  *
  *     usage: cp1 src dest
  */
-#include        <stdio.h>
-#include        <unistd.h>
-#include        <fcntl.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
-#define BUFFERSIZE      4096
-#define COPYMODE        0644
+#define BUFFERSIZE 4096
+#define COPYMODE 0644
 
 void oops(char *, char *);
 
-main(int ac, char *av[])
-{
-        int     in_fd, out_fd, n_chars;
-        char    buf[BUFFERSIZE];
-						/* check args 	*/
-        if ( ac != 3 ){
-                fprintf( stderr, "usage: %s source destination\n", *av);
-                exit(1);
-        }
-						/* open files	*/
+main(int ac, char *av[]) {
+  int in_fd, out_fd, n_chars;
+  char buf[BUFFERSIZE];
+  /* check args 	*/
+  if (ac != 3) {
+      fprintf(stderr, "usage: %s source destination\n", *av);
+      exit(1);
+  }
+  /* open files	*/
 
-        if ( (in_fd=open(av[1], O_RDONLY)) == -1 )
-                oops("Cannot open ", av[1]);
+  if ((in_fd = open(av[1], O_RDONLY)) == -1)
+    oops("Cannot open ", av[1]);
 
-        if ( (out_fd=creat( av[2], COPYMODE)) == -1 )
-                oops( "Cannot creat", av[2]);
-	
-						/* copy files	*/
+  if ((out_fd = creat(av[2], COPYMODE)) == -1)
+    oops("Cannot creat", av[2]);
 
-        while ( (n_chars = read(in_fd , buf, BUFFERSIZE)) > 0 )
-                if ( write( out_fd, buf, n_chars ) != n_chars )
-                        oops("Write error to ", av[2]);
-	if ( n_chars == -1 )
-			oops("Read error from ", av[1]);
+  /* copy files	*/
 
-						/* close files	*/
+  while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0)
+    if (write(out_fd, buf, n_chars) != n_chars)
+      oops("Write error to ", av[2]);
+  if (n_chars == -1)
+    oops("Read error from ", av[1]);
 
-        if ( close(in_fd) == -1 || close(out_fd) == -1 )
-                oops("Error closing files","");
+  /* close files	*/
+
+  if (close(in_fd) == -1 || close(out_fd) == -1)
+    oops("Error closing files", "");
 }
 
-void oops(char *s1, char *s2)
-{
-        fprintf(stderr,"Error: %s ", s1);
-        perror(s2);
-        exit(1);
+void oops(char *s1, char *s2) {
+  fprintf(stderr, "Error: %s ", s1);
+  perror(s2);
+  exit(1);
 }
-
