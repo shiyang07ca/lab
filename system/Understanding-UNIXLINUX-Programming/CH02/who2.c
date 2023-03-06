c7ba96c2e6dd0cceaaecdbad1ab0328ba0973fda
shiyang07ca <shiyang07ca@gmail.com>
2023-03-06 13:56:52 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH02/who2.c
+++ system/Understanding-UNIXLINUX-Programming/CH02/who2.c
@@ -2,31 +2,30 @@
  *         - suppresses empty records
  *         - formats time nicely
  */
-#include        <stdio.h>
-#include        <unistd.h>
-#include        <utmp.h>
-#include        <fcntl.h>
-#include        <time.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <utmp.h>
 
 /* #define      SHOWHOST */
 
 void showtime(long);
 void show_info(struct utmp *);
 
-int main()
-{
-        struct utmp     utbuf;          /* read info into here */
-        int             utmpfd;         /* read from this descriptor */
+int main() {
+    struct utmp utbuf; /* read info into here */
+    int utmpfd;        /* read from this descriptor */
 
-        if ( (utmpfd = open(UTMP_FILE, O_RDONLY)) == -1 ){
-                perror(UTMP_FILE);
-                exit(1);
-        }
+    if ((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1) {
+        perror(UTMP_FILE);
+        exit(1);
+    }
 
-        while( read(utmpfd, &utbuf, sizeof(utbuf)) == sizeof(utbuf) )
-                show_info( &utbuf );
-        close(utmpfd);
-        return 0;
+    while (read(utmpfd, &utbuf, sizeof(utbuf)) == sizeof(utbuf))
+        show_info(&utbuf);
+  close(utmpfd);
+  return 0;
 }
 /*
  *      show info()
@@ -34,24 +33,23 @@ int main()
  *                      in human readable form
  *                      * displays nothing if record has no user name
  */
-void show_info( struct utmp *utbufp )
-{
-        if ( utbufp->ut_type != USER_PROCESS )
-                return;
+void show_info(struct utmp *utbufp) {
+  if (utbufp->ut_type != USER_PROCESS)
+    return;
 
-        printf("%-8.8s", utbufp->ut_name);      /* the logname  */
-        printf(" ");                            /* a space      */
-        printf("%-8.8s", utbufp->ut_line);      /* the tty      */
-        printf(" ");                            /* a space      */
-        showtime( utbufp->ut_time );            /* display time */
+  printf("%-8.8s", utbufp->ut_name); /* the logname  */
+  printf(" ");                       /* a space      */
+  printf("%-8.8s", utbufp->ut_line); /* the tty      */
+  printf(" ");                       /* a space      */
+  showtime(utbufp->ut_time);         /* display time */
 #ifdef SHOWHOST
-        if ( utbufp->ut_host[0] != '\0' )
-                printf(" (%s)", utbufp->ut_host);/* the host    */
+  if (utbufp->ut_host[0] != '\0')
+    printf(" (%s)", utbufp->ut_host); /* the host    */
 #endif
-        printf("\n");                          /* newline      */
+  printf("\n"); /* newline      */
 }
 
-void showtime( long timeval )
+void showtime(long timeval)
 /*
  *      displays time in a format fit for human consumption
  *      uses ctime to build a string then picks parts out of it
@@ -59,11 +57,11 @@ void showtime( long timeval )
  *      it to 12chars.
  */
 {
-        char    *cp;                    /* to hold address of time      */
+  char *cp; /* to hold address of time      */
 
-        cp = ctime(&timeval);           /* convert time to string       */
-                                        /* string looks like            */
-                                        /* Mon Feb  4 00:46:40 EST 1991 */
-                                        /* 0123456789012345.            */
-        printf("%12.12s", cp+4 );       /* pick 12 chars from pos 4     */
+  cp = ctime(&timeval);      /* convert time to string       */
+                             /* string looks like            */
+                             /* Mon Feb  4 00:46:40 EST 1991 */
+                             /* 0123456789012345.            */
+  printf("%12.12s", cp + 4); /* pick 12 chars from pos 4     */
 }
