83ccd6874862c6daef479ba664733f9ad60eef18
shiyang07ca <shiyang07ca@gmail.com>
2023-03-08 23:38:19 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH03/fileinfo.c
+++ system/Understanding-UNIXLINUX-Programming/CH03/fileinfo.c
@@ -12,25 +12,27 @@ int main(int ac, char *av[]) {
   struct stat info; /* buffer for file info */
 
   if (ac > 1) {
-      if (stat(av[1], &info) != -1) {
-          show_stat_info(av[1], &info);
-          return 0;
-      } else {
-          perror(av[1]); /* report stat() errors  */
-      }
+    if (stat(av[1], &info) != -1) {
+      show_stat_info(av[1], &info);
+      return 0;
+    } else {
+      perror(av[1]); /* report stat() errors  */
+    }
   }
 
   return 1;
 }
+
 void show_stat_info(char *fname, struct stat *buf)
 /*
  * displays some info from stat in a name=value format
  */
+
 {
-    printf("   mode: %o\n", buf->st_mode);   /* type + mode */
-    printf("  links: %d\n", buf->st_nlink);  /* # links     */
-    printf("   user: %d\n", buf->st_uid);    /* user id     */
-    printf("  group: %d\n", buf->st_gid);    /* group id    */
+  printf("   mode: %o\n", buf->st_mode);   /* type + mode */
+  printf("  links: %d\n", buf->st_nlink);  /* # links     */
+  printf("   user: %d\n", buf->st_uid);    /* user id     */
+  printf("  group: %d\n", buf->st_gid);    /* group id    */
   printf("   size: %lld\n", buf->st_size); /* file size   */
   printf("modtime: %ld\n", buf->st_mtime); /* modified    */
   printf("   name: %s\n", fname);          /* filename    */
