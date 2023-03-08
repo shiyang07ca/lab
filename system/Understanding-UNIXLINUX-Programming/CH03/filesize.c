7e59c37c8d574f71dd7501a5b43c4779e09e028b
shiyang07ca <shiyang07ca@gmail.com>
2023-03-08 14:06:25 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH03/filesize.c
+++ system/Understanding-UNIXLINUX-Programming/CH03/filesize.c
@@ -3,12 +3,11 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-int main()
-{
-	struct stat infobuf;              /* place to store info */
+int main() {
+    struct stat infobuf; /* place to store info */
 
-	if ( stat( "/etc/passwd", &infobuf) == -1 )  /* get info */
-		perror("/etc/passwd");
-	else
-		printf(" The size of /etc/passwd is %d\n", infobuf.st_size );
+    if (stat("/etc/passwd", &infobuf) == -1) /* get info */
+        perror("/etc/passwd");
+    else
+        printf(" The size of /etc/passwd is %ld\n", infobuf.st_size);
 }
