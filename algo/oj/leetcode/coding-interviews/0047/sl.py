1dc327e9dec126bb2f64f5ee79f2f04cff62f0b4
shiyang07ca <shiyang07ca@gmail.com>
2023-03-08 08:49:35 +0800

    ci 47

---
+++ algo/oj/leetcode/coding-interviews/0047/sl.py
@@ -0,0 +1,82 @@
+"""
+
+# template
+# tag: dp
+
+剑指 Offer 47. 礼物的最大价值
+
+在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+
+
+
+示例 1:
+
+输入:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+输出: 12
+解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+
+
+提示：
+
+0 < grid.length <= 200
+0 < grid[0].length <= 200
+
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+
+from itertools import *
+from collections import *
+from copy import *
+from typing import *
+from math import *
+
+from os.path import abspath, join, dirname
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # oj
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+# https://leetcode.cn/problems/li-wu-de-zui-da-jie-zhi-lcof/solution/jiao-ni-yi-bu-bu-si-kao-dpcong-hui-su-da-epvl/
+
+
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        n = len(grid[0])
+        f = [0] * (n + 1)
+        for row in grid:
+            for j, x in enumerate(row):
+                f[j + 1] = max(f[j], f[j + 1]) + x
+        return f[n]
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
