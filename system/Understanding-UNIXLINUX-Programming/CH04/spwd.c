31604448ddf8b7447aadbfc412fd969413d27712
shiyang07ca <shiyang07ca@gmail.com>
2023-03-11 22:05:06 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH04/spwd.c
+++ system/Understanding-UNIXLINUX-Programming/CH04/spwd.c
@@ -8,86 +8,83 @@
  *
  *      bug: prints an empty string if run from "/"
  **/
-#include	<stdio.h>
-#include	<sys/types.h>
-#include	<sys/stat.h>
-#include	<dirent.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-ino_t	get_inode(char *);
-void    printpathto(ino_t);
-void    inum_to_name(ino_t , char *, int );
+ino_t get_inode(char *);
+void printpathto(ino_t);
+void inum_to_name(ino_t, char *, int);
 
-int main()
-{
-	printpathto( get_inode( "." ) );	/* print path to here	*/
-	putchar('\n');				/* then add newline	*/
-	return 0;
+int main() {
+    printpathto(get_inode(".")); /* print path to here	*/
+    putchar('\n');               /* then add newline	*/
+  return 0;
 }
 
-void printpathto( ino_t this_inode )
+void printpathto(ino_t this_inode)
 /*
  *	prints path leading down to an object with this inode
  *	kindof recursive
  */
 {
-	ino_t	my_inode ;
-	char	its_name[BUFSIZ];
+  ino_t my_inode;
+  char its_name[BUFSIZ];
 
-	if ( get_inode("..") != this_inode )
-	{
-		chdir( ".." );				/* up one dir	*/
+  if (get_inode("..") != this_inode) {
+    chdir(".."); /* up one dir	*/
 
-		inum_to_name(this_inode,its_name,BUFSIZ);/* get its name*/
+    inum_to_name(this_inode, its_name, BUFSIZ); /* get its name*/
 
-		my_inode = get_inode( "." );		/* print head	*/
-		printpathto( my_inode );		/* recursively	*/
-		printf("/%s", its_name );		/* now print	*/
-							/* name of this	*/
-	}
+    my_inode = get_inode("."); /* print head	*/
+    printpathto(my_inode);     /* recursively	*/
+    printf("/%s", its_name);   /* now print	*/
+                               /* name of this	*/
+  }
 }
 
-void inum_to_name(ino_t inode_to_find , char *namebuf, int buflen)
+void inum_to_name(ino_t inode_to_find, char *namebuf, int buflen)
 /*
  *	looks through current directory for a file with this inode
  *	number and copies its name into namebuf
  */
 {
-	DIR		*dir_ptr;		/* the directory */
-	struct dirent	*direntp;		/* each entry	 */
+  DIR *dir_ptr;           /* the directory */
+  struct dirent *direntp; /* each entry	 */
 
-	dir_ptr = opendir( "." );
-	if ( dir_ptr == NULL ){
-		perror( "." );
-		exit(1);
-	}
+  dir_ptr = opendir(".");
+  if (dir_ptr == NULL) {
+    perror(".");
+    exit(1);
+  }
 
-	/*
-	 * search directory for a file with specified inum
-	 */
+  /*
+   * search directory for a file with specified inum
+   */
 
-	while ( ( direntp = readdir( dir_ptr ) ) != NULL )
-		if ( direntp->d_ino == inode_to_find )
-		{
-			strncpy( namebuf, direntp->d_name, buflen);
-			namebuf[buflen-1] = '\0';   /* just in case */
-			closedir( dir_ptr );
-			return;
-		}
-	fprintf(stderr, "error looking for inum %d\n", inode_to_find);
-	exit(1);
+  while ((direntp = readdir(dir_ptr)) != NULL)
+    if (direntp->d_ino == inode_to_find) {
+      strncpy(namebuf, direntp->d_name, buflen);
+      namebuf[buflen - 1] = '\0'; /* just in case */
+      closedir(dir_ptr);
+      return;
+    }
+  fprintf(stderr, "error looking for inum %d\n", inode_to_find);
+  exit(1);
 }
 
-ino_t get_inode( char *fname )
+ino_t get_inode(char *fname)
 /*
  *	returns inode number of the file
  */
 {
-	struct stat info;
+  struct stat info;
 
-	if ( stat( fname , &info ) == -1 ){
-		fprintf(stderr, "Cannot stat ");
-		perror(fname);
-		exit(1);
-	}
-	return info.st_ino;
+  if (stat(fname, &info) == -1) {
+    fprintf(stderr, "Cannot stat ");
+    perror(fname);
+    exit(1);
+  }
+  return info.st_ino;
 }
