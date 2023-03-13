c91863cc1cda3f86705e63c4395aecbab0877729
shiyang07ca <shiyang07ca@gmail.com>
2023-03-13 13:48:11 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH05/write0.c
+++ system/Understanding-UNIXLINUX-Programming/CH05/write0.c
@@ -7,29 +7,31 @@
  *	  usage: write0 ttyname
  */
 
-#include	<stdio.h>
-#include	<fcntl.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-main( int ac, char *av[] )
-{
-	int	fd;
-	char	buf[BUFSIZ];
+void main(int ac, char *av[]) {
+  int fd;
+  char buf[BUFSIZ];
 
-	/* check args */
-	if ( ac != 2 ){
-		fprintf(stderr,"usage: write0 ttyname\n");
-		exit(1);
-	}
+  /* check args */
+  if (ac != 2) {
+      fprintf(stderr, "usage: write0 ttyname\n");
+      exit(1);
+  }
 
-	/* open devices */
-	fd = open( av[1], O_WRONLY );
-	if ( fd == -1 ){
-		perror(av[1]); exit(1);
-	}
+  /* open devices */
+  fd = open(av[1], O_WRONLY);
+  if (fd == -1) {
+    perror(av[1]);
+    exit(1);
+  }
 
-	/* loop until EOF on input */
-	while( fgets(buf, BUFSIZ, stdin) != NULL )
-		if ( write(fd, buf, strlen(buf)) == -1 )
-			break;
-	close( fd );
+  /* loop until EOF on input */
+  while (fgets(buf, BUFSIZ, stdin) != NULL)
+    if (write(fd, buf, strlen(buf)) == -1)
+        break;
+  close(fd);
 }
