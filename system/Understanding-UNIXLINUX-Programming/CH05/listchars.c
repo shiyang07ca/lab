8b08652369bfe0e8e855cb6ba8ab43ea8313da2e
shiyang07ca <shiyang07ca@gmail.com>
2023-03-14 13:43:26 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH05/listchars.c
+++ system/Understanding-UNIXLINUX-Programming/CH05/listchars.c
@@ -5,12 +5,11 @@
  *        notes: usesful to show that buffering/editing exists
  */
 
-#include        <stdio.h>
+#include <stdio.h>
 
-main()
-{
-        int     c, n = 0;
+void main() {
+  int c, n = 0;
 
-        while( ( c = getchar()) != 'Q' )
-                printf("char %3d is %c code %d\n", n++, c, c );
+  while ((c = getchar()) != 'Q')
+      printf("char %3d is %c code %d\n", n++, c, c);
 }
