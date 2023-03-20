e1dbd660e72435e94723a9d0106b7296d8a0c83b
shiyang07ca <shiyang07ca@gmail.com>
2023-03-20 13:45:43 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH07/sigdemo3.c
+++ system/Understanding-UNIXLINUX-Programming/CH07/sigdemo3.c
@@ -6,44 +6,40 @@
  *      question4: what happens to read() when a signal arrives?
  */
 
-#include	<stdio.h>
-#include	<signal.h>
+#include <signal.h>
+#include <stdio.h>
 
-#define	INPUTLEN	100
+#define INPUTLEN 100
 
-main(int ac, char *av[])
-{
-	void	inthandler(int);
-	void	quithandler(int);
-	char	input[INPUTLEN];
-	int	nchars;
+void main(int ac, char *av[]) {
+  void inthandler(int);
+  void quithandler(int);
+  char input[INPUTLEN];
+  int nchars;
 
-	signal( SIGINT,  inthandler );		/* set handler */
-	signal( SIGQUIT, quithandler );		/* set handler */
+  signal(SIGINT, inthandler);   /* set handler */
+  signal(SIGQUIT, quithandler); /* set handler */
 
-	do {
-		printf("\nType a message\n");
-		nchars = read(0, input, (INPUTLEN-1));
-		if ( nchars == -1 )
-			perror("read returned an error");
-		else {
-			input[nchars] = '\0';
-			printf("You typed: %s", input);
-		}
-	} 
-	while( strncmp( input , "quit" , 4 ) != 0 );
+  do {
+    printf("\nType a message\n");
+    nchars = read(0, input, (INPUTLEN - 1));
+    if (nchars == -1)
+      perror("read returned an error");
+    else {
+      input[nchars] = '\0';
+      printf("You typed: %s", input);
+    }
+  } while (strncmp(input, "quit", 4) != 0);
 }
 
-void inthandler(int s)
-{
-	printf(" Received signal %d .. waiting\n", s );
-	sleep(2);
-	printf("  Leaving inthandler \n");
+void inthandler(int s) {
+  printf(" Received signal %d .. waiting\n", s);
+  sleep(2);
+  printf("  Leaving inthandler \n");
 }
 
-void quithandler(int s)
-{
-	printf(" Received signal %d .. waiting\n", s );
-	sleep(3);
-	printf("  Leaving quithandler \n");
+void quithandler(int s) {
+  printf(" Received signal %d .. waiting\n", s);
+  sleep(3);
+  printf("  Leaving quithandler \n");
 }
