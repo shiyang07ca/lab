8021d9da7b1e901118b63987f9693f0e3abf7c0e
shiyang07ca <shiyang07ca@gmail.com>
2023-03-23 13:00:10 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH08/exec1.c
+++ system/Understanding-UNIXLINUX-Programming/CH08/exec1.c
@@ -1,14 +1,13 @@
 /* exec1.c - shows how easy it is for a program to run a program
  */
 
-main()
-{
-	char	*arglist[3];
+void main() {
+  char *arglist[3];
 
-	arglist[0] = "ls";
-	arglist[1] = "-l";
-	arglist[2] = 0 ;
-	printf("* * * About to exec ls -l\n");
-	execvp( "ls" , arglist );
-	printf("* * * ls is done. bye\n");
+  arglist[0] = "ls";
+  arglist[1] = "-l";
+  arglist[2] = 0;
+  printf("* * * About to exec ls -l\n");
+  execvp("ls", arglist);
+  printf("* * * ls is done. bye\n");
 }
