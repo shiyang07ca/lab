36ad78b3f6c33959524848e0421287b9bbbbc15d
shiyang07ca <shiyang07ca@gmail.com>
2023-03-26 20:47:25 +0800

    lc 1574

---
+++ algo/oj/leetcode/algo/01574/sl.py
@@ -0,0 +1,157 @@
+"""
+
+[1574] Shortest Subarray to be Removed to Make Array Sorted
+
+
+Given an integer array arr, remove a subarray (can be empty) from arr such that the remaining elements in arr are non-decreasing.
+
+Return the length of the shortest subarray to remove.
+
+A subarray is a contiguous subsequence of the array.
+
+
+Example 1:
+
+
+Input: arr = [1,2,3,10,4,2,3,5]
+Output: 3
+Explanation: The shortest subarray we can remove is [10,4,2] of length 3. The remaining elements after that will be [1,2,3,3,5] which are sorted.
+Another correct solution is to remove the subarray [3,10,4].
+
+
+Example 2:
+
+
+Input: arr = [5,4,3,2,1]
+Output: 4
+Explanation: Since the array is strictly decreasing, we can only keep a single element. Therefore we need to remove a subarray of length 4, either [5,4,3,2] or [4,3,2,1].
+
+
+Example 3:
+
+
+Input: arr = [1,2,3]
+Output: 0
+Explanation: The array is already non-decreasing. We do not need to remove any elements.
+
+
+
+Constraints:
+
+
+	1 <= arr.length <= 10⁵
+	0 <= arr[i] <= 10⁹
+
+################################################################
+
+# TODO
+
+1574. 删除最短的子数组使剩余数组有序
+
+给你一个整数数组 arr ，请你删除一个子数组（可以为空），使得 arr 中剩下的元素是 非递减 的。
+
+一个子数组指的是原数组中连续的一个子序列。
+
+请你返回满足题目要求的最短子数组的长度。
+
+
+
+示例 1：
+
+输入：arr = [1,2,3,10,4,2,3,5]
+输出：3
+解释：我们需要删除的最短子数组是 [10,4,2] ，长度为 3 。剩余元素形成非递减数组 [1,2,3,3,5] 。
+另一个正确的解为删除子数组 [3,10,4] 。
+
+
+示例 2：
+
+输入：arr = [5,4,3,2,1]
+输出：4
+解释：由于数组是严格递减的，我们只能保留一个元素。所以我们需要删除长度为 4 的子数组，要么删除 [5,4,3,2]，要么删除 [4,3,2,1]。
+
+
+示例 3：
+
+输入：arr = [1,2,3]
+输出：0
+解释：数组已经是非递减的了，我们不需要删除任何元素。
+
+
+示例 4：
+
+输入：arr = [1]
+输出：0
+
+
+提示：
+
+1 <= arr.length <= 10^5
+0 <= arr[i] <= 10^9
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+
+from itertools import *
+from collections import *
+from copy import *
+from typing import *
+from math import *
+
+from os.path import abspath, join, dirname
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # oj
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+# 作者：灵茶山艾府
+# 链接：https://leetcode.cn/problems/shortest-subarray-to-be-removed-to-make-array-sorted/solutions/2189149/dong-hua-yi-xie-jiu-cuo-liang-chong-xie-iijwz/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        n = len(arr)
+        right = n - 1
+        while right and arr[right - 1] <= arr[right]:
+            right -= 1
+        if right == 0:  # arr 已经是非递减数组
+            return 0
+        # 此时 arr[right-1] > arr[right]
+        ans = right  # 删除 arr[:right]
+        left = 0  # 枚举 left
+        while left == 0 or arr[left - 1] <= arr[left]:
+            while right < n and arr[right] < arr[left]:
+                right += 1
+            # 此时 arr[left] <= arr[right]，删除 arr[left+1:right]
+            ans = min(ans, right - left - 1)
+            left += 1
+        return ans
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
