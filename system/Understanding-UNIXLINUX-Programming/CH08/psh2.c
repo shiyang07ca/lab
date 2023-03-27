0b1feffb0209af971821487ab803c921c59da9eb
shiyang07ca <shiyang07ca@gmail.com>
2023-03-27 13:29:39 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH08/psh2.c
+++ system/Understanding-UNIXLINUX-Programming/CH08/psh2.c
@@ -6,73 +6,70 @@
  **		New problem: shell catches signals.  Run vi, press ^c.
  **/
 
-#include	<stdio.h>
-#include	<signal.h>
+#include <signal.h>
+#include <stdio.h>
 
-#define	MAXARGS		20				/* cmdline args	*/
-#define	ARGLEN		100				/* token length	*/
+#define MAXARGS 20 /* cmdline args	*/
+#define ARGLEN 100 /* token length	*/
 
-main()
-{
-	char	*arglist[MAXARGS+1];		/* an array of ptrs	*/
-	int	numargs;			/* index into array	*/
-	char	argbuf[ARGLEN];			/* read stuff here	*/
-	char	*makestring();			/* malloc etc		*/
+int main() {
+  char *arglist[MAXARGS + 1]; /* an array of ptrs	*/
+  int numargs;                /* index into array	*/
+  char argbuf[ARGLEN];        /* read stuff here	*/
+  char *makestring();         /* malloc etc		*/
 
-	numargs = 0;
-	while ( numargs < MAXARGS )
-	{					
-		printf("Arg[%d]? ", numargs);
-		if ( fgets(argbuf, ARGLEN, stdin) && *argbuf != '\n' )
-			arglist[numargs++] = makestring(argbuf);
-		else
-		{
-			if ( numargs > 0 ){		/* any args?	*/
-				arglist[numargs]=NULL;	/* close list	*/
-				execute( arglist );	/* do it	*/
-				numargs = 0;		/* and reset	*/
-			}
-		}
-	}
-	return 0;
+  numargs = 0;
+  while (numargs < MAXARGS) {
+    printf("Arg[%d]? ", numargs);
+    if (fgets(argbuf, ARGLEN, stdin) && *argbuf != '\n')
+      arglist[numargs++] = makestring(argbuf);
+    else {
+      if (numargs > 0) {         /* any args?	*/
+        arglist[numargs] = NULL; /* close list	*/
+        execute(arglist);        /* do it	*/
+        numargs = 0;             /* and reset	*/
+      }
+    }
+  }
+  return 0;
 }
 
-execute( char *arglist[] )
+execute(char *arglist[])
 /*
  *	use fork and execvp and wait to do it
  */
 {
-	int	pid,exitstatus;				/* of child	*/
+  int pid, exitstatus; /* of child	*/
 
-	pid = fork();					/* make new process */
-	switch( pid ){
-		case -1:	
-			perror("fork failed");
-			exit(1);
-		case 0:
-			execvp(arglist[0], arglist);		/* do it */
-			perror("execvp failed");
-			exit(1);
-		default:
-			while( wait(&exitstatus) != pid )
-				;
-			printf("child exited with status %d,%d\n",
-					exitstatus>>8, exitstatus&0377);
-	}
+  pid = fork(); /* make new process */
+  switch (pid) {
+  case -1:
+    perror("fork failed");
+    exit(1);
+  case 0:
+    execvp(arglist[0], arglist); /* do it */
+    perror("execvp failed");
+    exit(1);
+  default:
+    while (wait(&exitstatus) != pid)
+      ;
+    printf("child exited with status %d,%d\n", exitstatus >> 8,
+           exitstatus & 0377);
+  }
 }
-char *makestring( char *buf )
+char *makestring(char *buf)
 /*
  * trim off newline and create storage for the string
  */
 {
-	char	*cp, *malloc();
+  char *cp, *malloc();
 
-	buf[strlen(buf)-1] = '\0';		/* trim newline	*/
-	cp = malloc( strlen(buf)+1 );		/* get memory	*/
-	if ( cp == NULL ){			/* or die	*/
-		fprintf(stderr,"no memory\n");
-		exit(1);
-	}
-	strcpy(cp, buf);		/* copy chars	*/
-	return cp;			/* return ptr	*/
+  buf[strlen(buf) - 1] = '\0';  /* trim newline	*/
+  cp = malloc(strlen(buf) + 1); /* get memory	*/
+  if (cp == NULL) {             /* or die	*/
+    fprintf(stderr, "no memory\n");
+    exit(1);
+  }
+  strcpy(cp, buf); /* copy chars	*/
+  return cp;       /* return ptr	*/
 }
