9f0eddc5e30b52481be587fb3154910ceea195c8
shiyang07ca <shiyang07ca@gmail.com>
2023-03-28 13:28:37 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH09/smsh1.c
+++ system/Understanding-UNIXLINUX-Programming/CH09/smsh1.c
@@ -4,31 +4,30 @@
  **		uses fork, exec, wait, and ignores signals
  **/
 
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<unistd.h>
-#include	<signal.h>
-#include	"smsh.h"
+#include "smsh.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define	DFL_PROMPT	"> "
+#define DFL_PROMPT "> "
 
-int main()
-{
-	char	*cmdline, *prompt, **arglist;
-	int	result;
-	void	setup();
+int main() {
+  char *cmdline, *prompt, **arglist;
+  int result;
+  void setup();
 
-	prompt = DFL_PROMPT ;
-	setup();
+  prompt = DFL_PROMPT;
+  setup();
 
-	while ( (cmdline = next_cmd(prompt, stdin)) != NULL ){
-		if ( (arglist = splitline(cmdline)) != NULL  ){
-			result = execute(arglist);
-			freelist(arglist);
-		}
-		free(cmdline);
-	}
-	return 0;
+  while ((cmdline = next_cmd(prompt, stdin)) != NULL) {
+    if ((arglist = splitline(cmdline)) != NULL) {
+      result = execute(arglist);
+      freelist(arglist);
+    }
+    free(cmdline);
+  }
+  return 0;
 }
 
 void setup()
@@ -37,13 +36,11 @@ void setup()
  * returns: nothing. calls fatal() if trouble
  */
 {
-	signal(SIGINT,  SIG_IGN);
-	signal(SIGQUIT, SIG_IGN);
+  signal(SIGINT, SIG_IGN);
+  signal(SIGQUIT, SIG_IGN);
 }
 
-void fatal(char *s1, char *s2, int n)
-{
-	fprintf(stderr,"Error: %s,%s\n", s1, s2);
-	exit(n);
+void fatal(char *s1, char *s2, int n) {
+  fprintf(stderr, "Error: %s,%s\n", s1, s2);
+  exit(n);
 }
-
