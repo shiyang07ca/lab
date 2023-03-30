f1683d0b61aa2616f7cceb067e4f8088675c7d17
shiyang07ca <shiyang07ca@gmail.com>
2023-03-30 13:57:23 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH09/smsh4.c
+++ system/Understanding-UNIXLINUX-Programming/CH09/smsh4.c
@@ -1,10 +1,10 @@
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<unistd.h>
-#include	<signal.h>
-#include	<sys/wait.h>
-#include	"smsh.h"
-#include	"varlib.h"
+#include "smsh.h"
+#include "varlib.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /**
  **	small-shell version 4
@@ -13,25 +13,24 @@
  **		uses fork, exec, wait, and ignores signals
  **/
 
-#define	DFL_PROMPT	"> "
+#define DFL_PROMPT "> "
 
-int main()
-{
-	char	*cmdline, *prompt, **arglist;
-	int	result, process(char **);
-	void	setup();
+int main() {
+  char *cmdline, *prompt, **arglist;
+  int result, process(char **);
+  void setup();
 
-	prompt = DFL_PROMPT ;
-	setup();
+  prompt = DFL_PROMPT;
+  setup();
 
-	while ( (cmdline = next_cmd(prompt, stdin)) != NULL ){
-		if ( (arglist = splitline(cmdline)) != NULL  ){
-			result = process(arglist);
-			freelist(arglist);
-		}
-		free(cmdline);
-	}
-	return 0;
+  while ((cmdline = next_cmd(prompt, stdin)) != NULL) {
+    if ((arglist = splitline(cmdline)) != NULL) {
+      result = process(arglist);
+      freelist(arglist);
+    }
+    free(cmdline);
+  }
+  return 0;
 }
 
 void setup()
@@ -40,15 +39,14 @@ void setup()
  * returns: nothing. calls fatal() if trouble
  */
 {
-	extern char **environ;
+  extern char **environ;
 
-	VLenviron2table(environ);
-	signal(SIGINT,  SIG_IGN);
-	signal(SIGQUIT, SIG_IGN);
+  VLenviron2table(environ);
+  signal(SIGINT, SIG_IGN);
+  signal(SIGQUIT, SIG_IGN);
 }
 
-void fatal(char *s1, char *s2, int n)
-{
-	fprintf(stderr,"Error: %s,%s\n", s1, s2);
-	exit(n);
+void fatal(char *s1, char *s2, int n) {
+  fprintf(stderr, "Error: %s,%s\n", s1, s2);
+  exit(n);
 }
