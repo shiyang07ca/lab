8d546ac2dee9105970b832126815a7316d5835f7
shiyang07ca <shiyang07ca@gmail.com>
2023-04-01 23:36:02 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH10/pipe.c
+++ system/Understanding-UNIXLINUX-Programming/CH10/pipe.c
@@ -1,4 +1,4 @@
-/* pipe.c		
+/* pipe.c
  *	* Demonstrates how to create a pipeline from one process to another
  *	* Takes two args, each a command, and connects
  *	  av[1]'s output to input of av[2]
@@ -8,53 +8,56 @@
  *	* uses execlp() since known number of args
  *	* Note: exchange child and parent and watch fun
  */
-#include	<stdio.h>
-#include	<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
-#define	oops(m,x)	{ perror(m); exit(x); }
+#define oops(m, x)                                                             \
+  {                                                                            \
+    perror(m);                                                                 \
+    exit(x);                                                                   \
+  }
 
-main(int ac, char **av)
-{
-	int	thepipe[2],			/* two file descriptors	*/
-		newfd,				/* useful for pipes	*/
-		pid;				/* and the pid		*/
+main(int ac, char **av) {
+  int thepipe[2], /* two file descriptors	*/
+      newfd,      /* useful for pipes	*/
+      pid;        /* and the pid		*/
 
-	if ( ac != 3 ){
-		fprintf(stderr, "usage: pipe cmd1 cmd2\n");
-		exit(1);
-	}
-	if ( pipe( thepipe ) == -1 )		/* get a pipe		*/
-		oops("Cannot get a pipe", 1);
+  if (ac != 3) {
+    fprintf(stderr, "usage: pipe cmd1 cmd2\n");
+    exit(1);
+  }
+  if (pipe(thepipe) == -1) /* get a pipe		*/
+    oops("Cannot get a pipe", 1);
 
-	/* ------------------------------------------------------------ */
-	/*	now we have a pipe, now let's get two processes		*/
+  /* ------------------------------------------------------------ */
+  /*	now we have a pipe, now let's get two processes		*/
 
-	if ( (pid = fork()) == -1 )			/* get a proc	*/
-		oops("Cannot fork", 2);
+  if ((pid = fork()) == -1) /* get a proc	*/
+    oops("Cannot fork", 2);
 
-	/* ------------------------------------------------------------ */
-	/* 	Right Here, there are two processes			*/
-	/*             parent will read from pipe			*/
+  /* ------------------------------------------------------------ */
+  /* 	Right Here, there are two processes			*/
+  /*             parent will read from pipe			*/
 
-	if ( pid > 0 ){			/* parent will exec av[2]	*/
-		close(thepipe[1]);	/* parent doesn't write to pipe	*/
+  if (pid > 0) {       /* parent will exec av[2]	*/
+    close(thepipe[1]); /* parent doesn't write to pipe	*/
 
-		if ( dup2(thepipe[0], 0) == -1 )
-			oops("could not redirect stdin",3);
+    if (dup2(thepipe[0], 0) == -1)
+      oops("could not redirect stdin", 3);
 
-		close(thepipe[0]);	/* stdin is duped, close pipe	*/
-		execlp( av[2], av[2], NULL);
-		oops(av[2], 4);
-	}
+    close(thepipe[0]); /* stdin is duped, close pipe	*/
+    execlp(av[2], av[2], NULL);
+    oops(av[2], 4);
+  }
 
-	/*	 child execs av[1] and writes into pipe			*/
+  /*	 child execs av[1] and writes into pipe			*/
 
-	close(thepipe[0]);		/* child doesn't read from pipe	*/
+  close(thepipe[0]); /* child doesn't read from pipe	*/
 
-	if ( dup2(thepipe[1], 1) == -1 )
-		oops("could not redirect stdout", 4);
+  if (dup2(thepipe[1], 1) == -1)
+    oops("could not redirect stdout", 4);
 
-	close(thepipe[1]);		/* stdout is duped, close pipe	*/
-	execlp( av[1], av[1], NULL);
-	oops(av[1], 5);
+  close(thepipe[1]); /* stdout is duped, close pipe	*/
+  execlp(av[1], av[1], NULL);
+  oops(av[1], 5);
 }
