52e6a8bf399204f54ea8cb0ef09d4b33cffb3227
shiyang07ca <shiyang07ca@gmail.com>
2023-04-02 09:32:46 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH10/listargs.c
+++ system/Understanding-UNIXLINUX-Programming/CH10/listargs.c
@@ -2,15 +2,14 @@
  *              print the number of command line args, list the args,
  *		then print a message to stderr
  */
-#include	<stdio.h>
+#include <stdio.h>
 
-main( int ac, char *av[] )
-{
-	int	i;
+void main(int ac, char *av[]) {
+  int i;
 
-	printf("Number of args: %d, Args are:\n", ac);
-	for(i=0;i<ac;i++)
-		printf("args[%d] %s\n", i, av[i]);
+  printf("Number of args: %d, Args are:\n", ac);
+  for (i = 0; i < ac; i++)
+      printf("args[%d] %s\n", i, av[i]);
 
-	fprintf(stderr,"This message is sent to stderr.\n");
+  fprintf(stderr, "This message is sent to stderr.\n");
 }
