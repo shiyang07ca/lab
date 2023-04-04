8b8509c92965927e66d599b5331d0070777aafd0
shiyang07ca <shiyang07ca@gmail.com>
2023-04-04 13:51:08 +0800

    x

---
+++ algo/oj/leetcode/algo/01000/sl.py
@@ -0,0 +1,111 @@
+"""
+
+# TODO
+
+# tag: dp
+
+1000. 合并石头的最低成本
+
+
+有 N 堆石头排成一排，第 i 堆中有 stones[i] 块石头。
+
+每次移动（move）需要将连续的 K 堆石头合并为一堆，而这个移动的成本为这 K 堆石头的总数。
+
+找出把所有石头合并成一堆的最低成本。如果不可能，返回 -1 。
+
+
+示例 1：
+
+输入：stones = [3,2,4,1], K = 2
+输出：20
+解释：
+从 [3, 2, 4, 1] 开始。
+合并 [3, 2]，成本为 5，剩下 [5, 4, 1]。
+合并 [4, 1]，成本为 5，剩下 [5, 5]。
+合并 [5, 5]，成本为 10，剩下 [10]。
+总成本 20，这是可能的最小值。
+
+
+示例 2：
+
+输入：stones = [3,2,4,1], K = 3
+输出：-1
+解释：任何合并操作后，都会剩下 2 堆，我们无法再进行合并。所以这项任务是不可能完成的。.
+
+
+示例 3：
+
+输入：stones = [3,5,1,2,6], K = 3
+输出：25
+解释：
+从 [3, 5, 1, 2, 6] 开始。
+合并 [5, 1, 2]，成本为 8，剩下 [3, 8, 6]。
+合并 [3, 8, 6]，成本为 17，剩下 [17]。
+总成本 25，这是可能的最小值。
+
+
+提示：
+
+1 <= stones.length <= 30
+2 <= K <= 30
+1 <= stones[i] <= 100
+
+
+"""
+
+import sys
+import inspect
+import os
+import unittest
+
+from itertools import *
+from collections import *
+from copy import *
+from typing import *
+from math import *
+
+from os.path import abspath, join, dirname
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)  # algo
+parentdir = os.path.dirname(parentdir)  # leetcode
+parentdir = os.path.dirname(parentdir)  # oj
+parentdir = os.path.dirname(parentdir)  # algo
+sys.path.insert(0, parentdir)
+# print(sys.path)
+
+
+from algo.tree.builder import *
+
+
+
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sl = Solution()
+
+    def test_sl(self):
+"""
+[3,2,4,1]
+2
+[3,2,4,1]
+3
+[3,5,1,2,6]
+3
+[1]
+2
+[69,39,79,78,16,6,36,97,79,27,14,31,4]
+2
+[29,59,31,7,51,99,47,40,24,20,98,41,42,81,92,55]
+2
+"""
+        self.assertEqual(
+            self.sl,
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
