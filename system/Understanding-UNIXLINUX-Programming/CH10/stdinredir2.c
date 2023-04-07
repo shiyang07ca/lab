8d070db62b2fd6a00f4591c23670f7774eb920f8
shiyang07ca <shiyang07ca@gmail.com>
2023-04-07 13:14:13 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH10/stdinredir2.c
+++ system/Understanding-UNIXLINUX-Programming/CH10/stdinredir2.c
@@ -2,41 +2,51 @@
  * 	shows two more methods for redirecting standard input
  *	use #define to set one or the other
  */
-#include	<stdio.h>
-#include	<fcntl.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/* #define	CLOSE_DUP		/* open, close, dup, close */
-/* #define	USE_DUP2	/* open, dup2, close */
+/* open, close, dup, close
+/* #define	CLOSE_DUP */
 
-main()
-{
-	int	fd ;
-	int	newfd;
-	char	line[100];
+/* open, dup2, close */
+#define	USE_DUP2
 
-	/* read and print three lines */
+void main() {
+  int fd;
+  int newfd;
+  char line[100];
 
-	fgets( line, 100, stdin ); printf("%s", line );
-	fgets( line, 100, stdin ); printf("%s", line );
-	fgets( line, 100, stdin ); printf("%s", line );
+  /* read and print three lines */
 
-	/* redirect input */
-	fd = open("data", O_RDONLY);	/* open the disk file	*/
+  fgets(line, 100, stdin);
+  printf("%s", line);
+  fgets(line, 100, stdin);
+  printf("%s", line);
+  fgets(line, 100, stdin);
+  printf("%s", line);
+
+  /* redirect input */
+  fd = open("/etc/passwd", O_RDONLY); /* open the disk file	*/
 #ifdef CLOSE_DUP
-	close(0);
-	newfd = dup(fd);		/* copy open fd to 0	*/
+  close(0);
+  newfd = dup(fd); /* copy open fd to 0	*/
 #else
-	newfd = dup2(fd,0);		/* close 0, dup fd to 0 */
+  newfd = dup2(fd, 0); /* close 0, dup fd to 0 */
 #endif
-	if ( newfd != 0 ){
-		fprintf(stderr,"Could not duplicate fd to 0\n");
-		exit(1);
-	}
-	close(fd);			/* close original fd	*/
+  if (newfd != 0) {
+    fprintf(stderr, "Could not duplicate fd to 0\n");
+    exit(1);
+  }
+  close(fd); /* close original fd	*/
 
-	/* read and print three lines */
+  /* read and print three lines */
 
-	fgets( line, 100, stdin ); printf("%s", line );
-	fgets( line, 100, stdin ); printf("%s", line );
-	fgets( line, 100, stdin ); printf("%s", line );
+  fgets(line, 100, stdin);
+  printf("%s", line);
+  fgets(line, 100, stdin);
+  printf("%s", line);
+  fgets(line, 100, stdin);
+  printf("%s", line);
 }
