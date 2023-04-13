757fbc14cb59bdcf53990726ca24f52fa2a943e0
shiyang07ca <shiyang07ca@gmail.com>
2023-04-13 14:00:07 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH12/webserv.c
+++ system/Understanding-UNIXLINUX-Programming/CH12/webserv.c
@@ -7,41 +7,44 @@
  *             has many other weaknesses, but is a good start
  *      build: cc webserv.c socklib.c -o webserv
  */
-#include	<stdio.h>
-#include	<sys/types.h>
-#include	<sys/stat.h>
-#include	<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-main(int ac, char *av[])
-{
-	int 	sock, fd;
-	FILE	*fpin;
-	char	request[BUFSIZ];
+void main(int ac, char *av[]) {
+  int sock, fd;
+  FILE *fpin;
+  char request[BUFSIZ];
 
-	if ( ac == 1 ){
-		fprintf(stderr,"usage: ws portnum\n");
-		exit(1);
-	}
-	sock = make_server_socket( atoi(av[1]) );
-	if ( sock == -1 ) exit(2);
+  if (ac == 1) {
+    fprintf(stderr, "usage: ws portnum\n");
+    exit(1);
+  }
+  sock = make_server_socket(atoi(av[1]));
+  if (sock == -1)
+    exit(2);
 
-	/* main loop here */
+  /* main loop here */
 
-	while(1){
-		/* take a call and buffer it */
-		fd = accept( sock, NULL, NULL );
-		fpin = fdopen(fd, "r" );
+  while (1) {
+    /* take a call and buffer it */
+    fd = accept(sock, NULL, NULL);
+    fpin = fdopen(fd, "r");
 
-		/* read request */
-		fgets(request,BUFSIZ,fpin);
-		printf("got a call: request = %s", request);
-		read_til_crnl(fpin);
+    /* read request */
+    fgets(request, BUFSIZ, fpin);
+    printf("got a call: request = %s", request);
+    read_til_crnl(fpin);
 
-		/* do what client asks */
-		process_rq(request, fd);
+    /* do what client asks */
+    process_rq(request, fd);
 
-		fclose(fpin);
-	}
+    fclose(fpin);
+  }
 }
 
 /* ------------------------------------------------------ *
@@ -49,42 +52,40 @@ main(int ac, char *av[])
    skip over all request info until a CRNL is seen
    ------------------------------------------------------ */
 
-read_til_crnl(FILE *fp)
-{
-	char	buf[BUFSIZ];
-	while( fgets(buf,BUFSIZ,fp) != NULL && strcmp(buf,"\r\n") != 0 )
-		;
+read_til_crnl(FILE *fp) {
+  char buf[BUFSIZ];
+  while (fgets(buf, BUFSIZ, fp) != NULL && strcmp(buf, "\r\n") != 0)
+    ;
 }
 
 /* ------------------------------------------------------ *
    process_rq( char *rq, int fd )
-   do what the request asks for and write reply to fd 
+   do what the request asks for and write reply to fd
    handles request in a new process
    rq is HTTP command:  GET /foo/bar.html HTTP/1.0
    ------------------------------------------------------ */
 
-process_rq( char *rq, int fd )
-{
-	char	cmd[BUFSIZ], arg[BUFSIZ];
+process_rq(char *rq, int fd) {
+  char cmd[BUFSIZ], arg[BUFSIZ];
 
-	/* create a new process and return if not the child */
-	if ( fork() != 0 )
-		return;
+  /* create a new process and return if not the child */
+  if (fork() != 0)
+    return;
 
-	strcpy(arg, "./");		/* precede args with ./ */
-	if ( sscanf(rq, "%s%s", cmd, arg+2) != 2 )
-		return;
+  strcpy(arg, "./"); /* precede args with ./ */
+  if (sscanf(rq, "%s%s", cmd, arg + 2) != 2)
+    return;
 
-	if ( strcmp(cmd,"GET") != 0 )
-		cannot_do(fd);
-	else if ( not_exist( arg ) )
-		do_404(arg, fd );
-	else if ( isadir( arg ) )
-		do_ls( arg, fd );
-	else if ( ends_in_cgi( arg ) )
-		do_exec( arg, fd );
-	else
-		do_cat( arg, fd );
+  if (strcmp(cmd, "GET") != 0)
+    cannot_do(fd);
+  else if (not_exist(arg))
+    do_404(arg, fd);
+  else if (isadir(arg))
+    do_ls(arg, fd);
+  else if (ends_in_cgi(arg))
+    do_exec(arg, fd);
+  else
+    do_cat(arg, fd);
 }
 
 /* ------------------------------------------------------ *
@@ -92,11 +93,10 @@ process_rq( char *rq, int fd )
    if content_type is NULL then don't send content type
    ------------------------------------------------------ */
 
-header( FILE *fp, char *content_type )
-{
-	fprintf(fp, "HTTP/1.0 200 OK\r\n");
-	if ( content_type )
-		fprintf(fp, "Content-type: %s\r\n", content_type );
+header(FILE *fp, char *content_type) {
+  fprintf(fp, "HTTP/1.0 200 OK\r\n");
+  if (content_type)
+    fprintf(fp, "Content-type: %s\r\n", content_type);
 }
 
 /* ------------------------------------------------------ *
@@ -105,29 +105,26 @@ header( FILE *fp, char *content_type )
     and do_404(item,fd)     no such object
    ------------------------------------------------------ */
 
-cannot_do(int fd)
-{
-	FILE	*fp = fdopen(fd,"w");
+cannot_do(int fd) {
+  FILE *fp = fdopen(fd, "w");
 
-	fprintf(fp, "HTTP/1.0 501 Not Implemented\r\n");
-	fprintf(fp, "Content-type: text/plain\r\n");
-	fprintf(fp, "\r\n");
+  fprintf(fp, "HTTP/1.0 501 Not Implemented\r\n");
+  fprintf(fp, "Content-type: text/plain\r\n");
+  fprintf(fp, "\r\n");
 
-	fprintf(fp, "That command is not yet implemented\r\n");
-	fclose(fp);
+  fprintf(fp, "That command is not yet implemented\r\n");
+  fclose(fp);
 }
 
-do_404(char *item, int fd)
-{
-	FILE	*fp = fdopen(fd,"w");
+int do_404(char *item, int fd) {
+  FILE *fp = fdopen(fd, "w");
 
-	fprintf(fp, "HTTP/1.0 404 Not Found\r\n");
-	fprintf(fp, "Content-type: text/plain\r\n");
-	fprintf(fp, "\r\n");
+  fprintf(fp, "HTTP/1.0 404 Not Found\r\n");
+  fprintf(fp, "Content-type: text/plain\r\n");
+  fprintf(fp, "\r\n");
 
-	fprintf(fp, "The item you requested: %s\r\nis not found\r\n", 
-			item);
-	fclose(fp);
+  fprintf(fp, "The item you requested: %s\r\nis not found\r\n", item);
+  fclose(fp);
 }
 
 /* ------------------------------------------------------ *
@@ -136,33 +133,30 @@ do_404(char *item, int fd)
    do_ls runs ls. It should not
    ------------------------------------------------------ */
 
-isadir(char *f)
-{
-	struct stat info;
-	return ( stat(f, &info) != -1 && S_ISDIR(info.st_mode) );
+int isadir(char *f) {
+  struct stat info;
+  return (stat(f, &info) != -1 && S_ISDIR(info.st_mode));
 }
 
-not_exist(char *f)
-{
-	struct stat info;
-	return( stat(f,&info) == -1 );
+int not_exist(char *f) {
+  struct stat info;
+  return (stat(f, &info) == -1);
 }
 
-do_ls(char *dir, int fd)
-{
-	FILE	*fp ;
+int do_ls(char *dir, int fd) {
+  FILE *fp;
 
-	fp = fdopen(fd,"w");
-	header(fp, "text/plain");
-	fprintf(fp,"\r\n");
-	fflush(fp);
+  fp = fdopen(fd, "w");
+  header(fp, "text/plain");
+  fprintf(fp, "\r\n");
+  fflush(fp);
 
-	dup2(fd,1);
-	dup2(fd,2);
-	close(fd);
-	execlp("ls","ls","-l",dir,NULL);
-	perror(dir);
-	exit(1);
+  dup2(fd, 1);
+  dup2(fd, 2);
+  close(fd);
+  execlp("ls", "ls", "-l", dir, NULL);
+  perror(dir);
+  exit(1);
 }
 
 /* ------------------------------------------------------ *
@@ -170,64 +164,58 @@ do_ls(char *dir, int fd)
    one to run the program.
    ------------------------------------------------------ */
 
-char * file_type(char *f)
+char *file_type(char *f)
 /* returns 'extension' of file */
 {
-	char	*cp;
-	if ( (cp = strrchr(f, '.' )) != NULL )
-		return cp+1;
-	return "";
+  char *cp;
+  if ((cp = strrchr(f, '.')) != NULL)
+    return cp + 1;
+  return "";
 }
 
-ends_in_cgi(char *f)
-{
-	return ( strcmp( file_type(f), "cgi" ) == 0 );
-}
+ends_in_cgi(char *f) { return (strcmp(file_type(f), "cgi") == 0); }
 
-do_exec( char *prog, int fd )
-{
-	FILE	*fp ;
+do_exec(char *prog, int fd) {
+  FILE *fp;
 
-	fp = fdopen(fd,"w");
-	header(fp, NULL);
-	fflush(fp);
-	dup2(fd, 1);
-	dup2(fd, 2);
-	close(fd);
-	execl(prog,prog,NULL);
-	perror(prog);
+  fp = fdopen(fd, "w");
+  header(fp, NULL);
+  fflush(fp);
+  dup2(fd, 1);
+  dup2(fd, 2);
+  close(fd);
+  execl(prog, prog, NULL);
+  perror(prog);
 }
 /* ------------------------------------------------------ *
    do_cat(filename,fd)
    sends back contents after a header
    ------------------------------------------------------ */
 
-do_cat(char *f, int fd)
-{
-	char	*extension = file_type(f);
-	char	*content = "text/plain";
-	FILE	*fpsock, *fpfile;
-	int	c;
+do_cat(char *f, int fd) {
+  char *extension = file_type(f);
+  char *content = "text/plain";
+  FILE *fpsock, *fpfile;
+  int c;
 
-	if ( strcmp(extension,"html") == 0 )
-		content = "text/html";
-	else if ( strcmp(extension, "gif") == 0 )
-		content = "image/gif";
-	else if ( strcmp(extension, "jpg") == 0 )
-		content = "image/jpeg";
-	else if ( strcmp(extension, "jpeg") == 0 )
-		content = "image/jpeg";
+  if (strcmp(extension, "html") == 0)
+    content = "text/html";
+  else if (strcmp(extension, "gif") == 0)
+    content = "image/gif";
+  else if (strcmp(extension, "jpg") == 0)
+    content = "image/jpeg";
+  else if (strcmp(extension, "jpeg") == 0)
+    content = "image/jpeg";
 
-	fpsock = fdopen(fd, "w");
-	fpfile = fopen( f , "r");
-	if ( fpsock != NULL && fpfile != NULL )
-	{
-		header( fpsock, content );
-		fprintf(fpsock, "\r\n");
-		while( (c = getc(fpfile) ) != EOF )
-			putc(c, fpsock);
-		fclose(fpfile);
-		fclose(fpsock);
-	}
-	exit(0);
+  fpsock = fdopen(fd, "w");
+  fpfile = fopen(f, "r");
+  if (fpsock != NULL && fpfile != NULL) {
+    header(fpsock, content);
+    fprintf(fpsock, "\r\n");
+    while ((c = getc(fpfile)) != EOF)
+      putc(c, fpsock);
+    fclose(fpfile);
+    fclose(fpsock);
+  }
+  exit(0);
 }
