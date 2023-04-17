458428c1a17930b74985b39487689ea4d3aa764d
shiyang07ca <shiyang07ca@gmail.com>
2023-04-17 14:15:29 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH13/logfilec.c
+++ system/Understanding-UNIXLINUX-Programming/CH13/logfilec.c
@@ -3,35 +3,39 @@
  *              usage: logfilec "a message here"
  */
 
-#include	<stdio.h>
-#include	<sys/types.h>
-#include	<sys/socket.h>
-#include	<sys/un.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
 
-#define	SOCKET	"/tmp/logfilesock"
-#define	oops(m,x) { perror(m); exit(x); }
+#define SOCKET "/tmp/logfilesock"
+#define oops(m, x)                                                             \
+  {                                                                            \
+    perror(m);                                                                 \
+    exit(x);                                                                   \
+  }
 
-main(int ac, char *av[])
-{
-	int	           sock;
-	struct sockaddr_un addr;
-	socklen_t          addrlen;
-	char	           sockname[] = SOCKET ;
-	char               *msg = av[1];
+int main(int ac, char *av[]) {
+  int sock;
+  struct sockaddr_un addr;
+  socklen_t addrlen;
+  char sockname[] = SOCKET;
+  char *msg = av[1];
 
-	if ( ac != 2 ){
-		fprintf(stderr,"usage: logfilec 'message'\n");
-		exit(1);
-	}
-	sock = socket(PF_UNIX, SOCK_DGRAM, 0);
-	if ( sock == -1 )
-		oops("socket",2);
+  if (ac != 2) {
+    fprintf(stderr, "usage: logfilec 'message'\n");
+    exit(1);
+  }
+  sock = socket(PF_UNIX, SOCK_DGRAM, 0);
+  if (sock == -1)
+    oops("socket", 2);
 
-	addr.sun_family = AF_UNIX;
-	strcpy(addr.sun_path, sockname);
-	addrlen = strlen(sockname) + sizeof(addr.sun_family) ;
+  addr.sun_family = AF_UNIX;
+  strcpy(addr.sun_path, sockname);
+  addrlen = strlen(sockname) + sizeof(addr.sun_family);
 
-	if ( sendto(sock,msg, strlen(msg), 0, 
-				(struct sockaddr *)&addr, addrlen) == -1 )
-		oops("sendto",3);
+  if (sendto(sock, msg, strlen(msg), 0, (struct sockaddr *)&addr, addrlen) ==
+      -1)
+    oops("sendto", 3);
 }
