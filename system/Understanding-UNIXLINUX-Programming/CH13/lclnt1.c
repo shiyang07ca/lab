0c2266f00ec152c4f8eb772355f8e440986e74f1
shiyang07ca <shiyang07ca@gmail.com>
2023-04-17 13:11:00 +0800

    x

--- system/Understanding-UNIXLINUX-Programming/CH13/lclnt1.c
+++ system/Understanding-UNIXLINUX-Programming/CH13/lclnt1.c
@@ -6,24 +6,20 @@
 
 #include <stdio.h>
 
-int main(int ac, char *av[])
-{
-	setup();
-	if (get_ticket() != 0 )
-		exit(0);	
+int main(int ac, char *av[]) {
+  setup();
+  if (get_ticket() != 0)
+    exit(0);
 
-	do_regular_work();
+  do_regular_work();
 
-	release_ticket();
-	shut_down();
-
+  release_ticket();
+  shut_down();
 }
 /****************************************************************************
  * do_regular_work  the main work of the application goes here
  */
-do_regular_work()
-{
-	printf("SuperSleep version 1.0 Running - Licensed Software\n");
-	sleep(10);	/* our patented sleep algorithm */
+void do_regular_work() {
+  printf("SuperSleep version 1.0 Running - Licensed Software\n");
+  sleep(10); /* our patented sleep algorithm */
 }
-
--- system/Understanding-UNIXLINUX-Programming/CH13/lserv1.c
+++ system/Understanding-UNIXLINUX-Programming/CH13/lserv1.c
@@ -1,37 +1,39 @@
 /****************************************************************************
- * lsrv1.c  
+ * lsrv1.c
  * License server server program version 1
  */
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/errno.h>
-#define MSGLEN		128		/* Size of our datagrams */
+#include <sys/socket.h>
+#include <sys/types.h>
+#define MSGLEN 128 /* Size of our datagrams */
 
-int main(int ac, char *av[])
-{
-	struct sockaddr_in client_addr;
-	socklen_t addrlen;
-	char      buf[MSGLEN];
-	int	  ret;
-	int       sock;
+int setup();
+void narrate(char *msg1, char *msg2, struct sockaddr_in *clientp);
+void handle_request(char *req, struct sockaddr_in *client, socklen_t addlen);
 
-	sock = setup();
+int main(int ac, char *av[]) {
+    struct sockaddr_in client_addr;
+    socklen_t addrlen;
+    char buf[MSGLEN];
+    int ret;
+    int sock;
 
-	while(1) {
-		addrlen = sizeof(client_addr);
-		ret = recvfrom(sock,buf,MSGLEN,0,
-				(struct sockaddr*)&client_addr,&addrlen);
-		if ( ret != -1 ){
-			buf[ret] = '\0';
-			narrate("GOT:", buf, &client_addr);
-			handle_request(buf,&client_addr,addrlen);
-		}
-		else if ( errno != EINTR )
-			perror("recvfrom");
-	}
+    sock = setup();
+
+    while (1) {
+        addrlen = sizeof(client_addr);
+        ret = recvfrom(sock, buf, MSGLEN, 0, (struct sockaddr *)&client_addr,
+                       &addrlen);
+        if (ret != -1) {
+            buf[ret] = '\0';
+            narrate("GOT:", buf, &client_addr);
+            handle_request(buf, &client_addr, addrlen);
+        } else if (errno != EINTR)
+            perror("recvfrom");
+    }
 }
-
