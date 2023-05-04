3450f5047f5e9c1de54a02258b437c40ebc850de
shiyang07ca <shiyang07ca@gmail.com>
2023-05-04 22:33:59 +0800

    x

--- algo/copypasta/monotone_queue.py
+++ algo/copypasta/monotone_queue.py
@@ -7,9 +7,12 @@
 TODO
 
 
-模板题 https://leetcode.cn/problems/dui-lie-de-zui-da-zhi-lcof/
+模板题
+剑指 Offer 59 - II. 队列的最大值 https://leetcode.cn/problems/dui-lie-de-zui-da-zhi-lcof/
 https://leetcode.cn/tag/monotonic-queue/problemset/
 
-LC2106. 摘水果 https://leetcode.cn/problems/maximum-fruits-harvested-after-at-most-k-steps/description/
+LC209 长度最小的子数组 https://leetcode.cn/problems/minimum-size-subarray-sum/
+
+LC2106 摘水果 https://leetcode.cn/problems/maximum-fruits-harvested-after-at-most-k-steps/description/
 
 """
