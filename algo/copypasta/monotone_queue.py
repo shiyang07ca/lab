51ee0f3278ffd1975e920a19d977893f9abc1554
shiyang07ca <shiyang07ca@gmail.com>
2023-06-12 13:49:09 +0800

    x

--- algo/copypasta/monotone_queue.py
+++ algo/copypasta/monotone_queue.py
@@ -4,10 +4,16 @@
 
 需要不断维护队列的单调性，时刻保证队列元素从大到小或从小到大
 前置知识：双指针
+
+https://oi-wiki.org/ds/monotonous-queue/
+https://oi-wiki.org/dp/opt/monotonous-queue-stack/
+https://cp-algorithms.com/data_structures/stack_queue_modification.html
+
 TODO
 
 
 模板题
+LC239 滑动窗口最大值 https://leetcode.cn/problems/sliding-window-maximum/
 剑指 Offer 59 - II. 队列的最大值 https://leetcode.cn/problems/dui-lie-de-zui-da-zhi-lcof/
 https://leetcode.cn/tag/monotonic-queue/problemset/
 
