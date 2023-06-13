49c522c753a38359be7336eac1e37226e1305eae
shiyang07ca <shiyang07ca@gmail.com>
2023-06-13 15:55:59 +0800

    x

--- pl/go/ioc.go
+++ pl/go/ioc.go
@@ -78,7 +78,26 @@ func t1() {
 	}
 }
 
+// ================================================================
+// 反转控制
+
+type IntSet struct {
+    data map[int]bool
+}
+func NewIntSet() IntSet {
+    return IntSet{make(map[int]bool)}
+}
+func (set *IntSet) Add(x int) {
+    set.data[x] = true
+}
+func (set *IntSet) Delete(x int) {
+    delete(set.data, x)
+}
+func (set *IntSet) Contains(x int) bool {
+    return set.data[x]
+}
+
 func main() {
-	t1()
+	// t1()
 
 }
