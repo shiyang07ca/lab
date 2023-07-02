11e3af72bed0b023b5ce091e57e76094d515a65d
shiyang07ca <shiyang07ca@gmail.com>
2023-07-02 08:21:50 +0800

    x

--- algo/oj/leetcode/python/00002/solution.py
+++ algo/oj/leetcode/python/00002/solution.py
@@ -87,7 +87,7 @@ class Solution:
         return ans.next
 
     # l1 和 l2 为当前遍历的节点，carry 为进位
-    def addTwoNumbers(
+    def addTwoNumbers2(
         self, l1: Optional[ListNode], l2: Optional[ListNode], carry=0
     ) -> Optional[ListNode]:
         if l1 is None and l2 is None:  # 递归边界：l1 和 l2 都是空节点
@@ -100,6 +100,22 @@ class Solution:
             l1.next, l2.next if l2 else None, carry // 10
         )  # 进位
         return l1
+
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        cur = dummy = ListNode()  # 哨兵节点
+        carry = 0  # 进位
+        while l1 or l2 or carry:  # 有一个不是空节点，或者还有进位，就继续迭代
+            carry += (l1.val if l1 else 0) + (l2.val if l2 else 0)  # 节点值和进位加在一起
+            cur.next = ListNode(carry % 10)  # 每个节点保存一个数位
+            carry //= 10  # 新的进位
+            cur = cur.next  # 下一个节点
+            if l1:
+                l1 = l1.next  # 下一个节点
+            if l2:
+                l2 = l2.next  # 下一个节点
+        return dummy.next  # 哨兵节点的下一个节点就是头节点
 
 
 # @lc code=end
