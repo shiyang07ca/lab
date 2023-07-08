408765c0850c104a05069fb54199805171609125
shiyang07ca <shiyang07ca@gmail.com>
2023-07-08 23:53:48 +0800

    x

--- algo/oj/leetcode/python/00167/solution.py
+++ algo/oj/leetcode/python/00167/solution.py
@@ -9,12 +9,24 @@ from leetgo_py import *
 
 
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum1(self, numbers: List[int], target: int) -> List[int]:
         n = len(numbers)
         for i, x in enumerate(numbers):
             j = bisect_left(numbers, target - x, lo=i + 1)
             if j < n and numbers[j] == target - x:
                 return [i + 1, j + 1]
+
+    # 链接：https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/solutions/2335244/python3javacgotypescript-yi-ti-shuang-ji-a8kx/
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            x = numbers[i] + numbers[j]
+            if x == target:
+                return [i + 1, j + 1]
+            if x < target:
+                i += 1
+            else:
+                j -= 1
 
 
 # @lc code=end
