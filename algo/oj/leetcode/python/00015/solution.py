b2c72b26e888f68ac73f28595e179b6ed4f85127
shiyang07ca <shiyang07ca@gmail.com>
2023-07-09 22:01:09 +0800

    lc 15

---
+++ algo/oj/leetcode/python/00015/solution.py
@@ -0,0 +1,45 @@
+# Created by shiyang07ca at 2023/07/09 21:29
+# leetgo: dev
+# https://leetcode.cn/problems/3sum/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+# 链接：https://leetcode.cn/problems/3sum/solutions/2336474/python3javacgotypescript-yi-ti-yi-jie-pa-7z00/
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        ans = []
+        for i in range(n - 2):
+            if nums[i] > 0:
+                break
+            if i and nums[i] == nums[i - 1]:
+                continue
+            j, k = i + 1, n - 1
+            while j < k:
+                x = nums[i] + nums[j] + nums[k]
+                if x < 0:
+                    j += 1
+                elif x > 0:
+                    k -= 1
+                else:
+                    ans.append([nums[i], nums[j], nums[k]])
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+        return ans
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    ans = Solution().threeSum(nums)
+
+    print("\noutput:", serialize(ans))
