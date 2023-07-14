6242923a2484496cbc7e0d5470d0751296cb2dd1
shiyang07ca <shiyang07ca@gmail.com>
2023-07-14 13:26:49 +0800

    lc 979 tag:TODO

--- algo/oj/leetcode/python/00979/solution.py
+++ algo/oj/leetcode/python/00979/solution.py
@@ -7,6 +7,9 @@ from leetgo_py import *
 
 # @lc code=begin
 
+# TODO
+# tag: dfs
+
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -14,9 +17,26 @@ from leetgo_py import *
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+# 链接：https://leetcode.cn/problems/distribute-coins-in-binary-tree/solutions/2343262/tu-jie-mei-you-si-lu-jin-lai-miao-dong-p-vrni/
 class Solution:
     def distributeCoins(self, root: Optional[TreeNode]) -> int:
-        pass
+        ans = 0
+
+        def dfs(node: Optional[TreeNode]) -> (int, int):
+            if node is None:
+                return 0, 0
+            coins_l, nodes_l = dfs(node.left)
+            coins_r, nodes_r = dfs(node.right)
+            coins = coins_l + coins_r + node.val  # 子树硬币个数
+            nodes = nodes_l + nodes_r + 1  # 子树节点数
+            nonlocal ans
+            ans += abs(coins - nodes)
+            return coins, nodes
+
+        dfs(root)
+        return ans
 
 
 # @lc code=end
