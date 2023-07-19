9d3f4a9995dfa8810ca894242f6a5df0bb89ca03
shiyang07ca <shiyang07ca@gmail.com>
2023-07-19 13:01:27 +0800

    x

--- algo/oj/leetcode/python/00874/solution.py
+++ algo/oj/leetcode/python/00874/solution.py
@@ -11,7 +11,7 @@ from leetgo_py import *
 
 
 class Solution:
-    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+    def robotSim1(self, commands: List[int], obstacles: List[List[int]]) -> int:
         ans = d = 0
         ds = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         cur = [0, 0]
@@ -47,6 +47,26 @@ class Solution:
                         cur[0] = x + dx * c
             ans = max(ans, cur[0] ** 2 + cur[1] ** 2)
             print(cur)
+        return ans
+
+    # 链接：https://leetcode.cn/problems/walking-robot-simulation/solutions/2349712/python3javacgotypescript-yi-ti-yi-jie-ha-ghjx/
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        dirs = (0, 1, 0, -1, 0)
+        s = {(x, y) for x, y in obstacles}
+        ans = k = 0
+        x = y = 0
+        for c in commands:
+            if c == -2:
+                k = (k + 3) % 4
+            elif c == -1:
+                k = (k + 1) % 4
+            else:
+                for _ in range(c):
+                    nx, ny = x + dirs[k], y + dirs[k + 1]
+                    if (nx, ny) in s:
+                        break
+                    x, y = nx, ny
+                    ans = max(ans, x * x + y * y)
         return ans
 
 
