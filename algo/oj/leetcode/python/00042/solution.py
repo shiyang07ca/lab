56afd053dff2454aae182b89143c48983ca91a9d
shiyang07ca <shiyang07ca@gmail.com>
2023-07-23 23:19:09 +0800

    x

--- algo/oj/leetcode/python/00042/solution.py
+++ algo/oj/leetcode/python/00042/solution.py
@@ -12,6 +12,21 @@ from leetgo_py import *
 
 class Solution:
     def trap(self, height: List[int]) -> int:
+        n = len(height)
+        pre_max = [height[0]] * n
+        for i in range(1, n):
+            pre_max[i] = max(pre_max[i - 1], height[i])
+
+        suf_max = [height[-1]] * n
+        for i in range(n - 2, -1, -1):
+            suf_max[i] = max(suf_max[i + 1], height[i])
+
+        ans = 0
+        for h, pre, suf in zip(height, pre_max, suf_max):
+            ans += min(pre, suf) - h
+        return ans
+
+    def trap2(self, height: List[int]) -> int:
         ans = left = pre_max = suf_max = 0
         right = len(height) - 1
         while left <= right:
