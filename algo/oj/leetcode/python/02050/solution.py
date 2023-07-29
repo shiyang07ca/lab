2405fbd97687a6c658b96e6b53d68a881650bfde
shiyang07ca <shiyang07ca@gmail.com>
2023-07-29 23:17:27 +0800

    x

--- algo/oj/leetcode/python/02050/solution.py
+++ algo/oj/leetcode/python/02050/solution.py
@@ -12,9 +12,34 @@ from leetgo_py import *
 # TODO
 
 
-# 链接：https://leetcode.cn/problems/parallel-courses-iii/solutions/1063928/tuo-bu-pai-xu-dong-tai-gui-hua-by-endles-dph6/
 class Solution:
+    # 链接：https://leetcode.cn/problems/parallel-courses-iii/solutions/2362108/python3javacgotypescript-yi-ti-yi-jie-tu-4nbv/
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        g = defaultdict(list)
+        indeg = [0] * n
+        for a, b in relations:
+            g[a - 1].append(b - 1)
+            indeg[b - 1] += 1
+        q = deque()
+        f = [0] * n
+        ans = 0
+        for i, (v, t) in enumerate(zip(indeg, time)):
+            if v == 0:
+                q.append(i)
+                f[i] = t
+                ans = max(ans, t)
+        while q:
+            i = q.popleft()
+            for j in g[i]:
+                f[j] = max(f[j], f[i] + time[j])
+                ans = max(ans, f[j])
+                indeg[j] -= 1
+                if indeg[j] == 0:
+                    q.append(j)
+        return ans
+
+    # 链接：https://leetcode.cn/problems/parallel-courses-iii/solutions/1063928/tuo-bu-pai-xu-dong-tai-gui-hua-by-endles-dph6/
+    def minimumTime2(self, n: int, relations: List[List[int]], time: List[int]) -> int:
         g = [[] for _ in range(n)]
         deg = [0] * n  # deg[i] 表示 i 的先修课的个数
         for x, y in relations:
