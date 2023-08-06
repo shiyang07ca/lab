503096b5747a756eb70c221ccdd12f9d7aea30b1
shiyang07ca <shiyang07ca@gmail.com>
2023-08-06 17:14:36 +0800

    x

---
+++ algo/copypasta/union_find.py
@@ -0,0 +1,19 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def find(self, a):
+        a = self.parent[a]
+        acopy = a
+        while a != self.parent[a]:
+            a = self.parent[a]
+        while acopy != a:
+            self.parent[acopy], acopy = a, self.parent[acopy]
+        return a
+
+    def merge(self, a, b):
+        pa, pb = self.find(a), self.find(b)
+        if pa == pb:
+            return False
+        self.parent[pb] = pa
+        return True
