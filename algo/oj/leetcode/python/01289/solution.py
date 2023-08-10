19cccee7b0aa118ea7b1331402c2cfd33017c7f0
shiyang07ca <shiyang07ca@gmail.com>
2023-08-10 13:58:02 +0800

    x

--- algo/oj/leetcode/python/01289/solution.py
+++ algo/oj/leetcode/python/01289/solution.py
@@ -29,23 +29,6 @@ class Solution:
                 f[i][j] = v + x
         return min(f[n])
 
-    def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        f = g = 0
-        fp = -1
-        for row in grid:
-            ff = gg = inf
-            ffp = -1
-            for j, v in enumerate(row):
-                s = (g if j == fp else f) + v
-                if s < ff:
-                    gg = ff
-                    ff = s
-                    ffp = j
-                elif s < gg:
-                    gg = s
-            f, g, fp = ff, gg, ffp
-        return f
-
 
 # @lc code=end
 
