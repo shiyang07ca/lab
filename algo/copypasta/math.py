ce5654df17035487ad7f194791a95bf6a82ebe75
shiyang07ca <shiyang07ca@gmail.com>
2023-08-14 13:57:20 +0800

    x

--- algo/copypasta/math.py
+++ algo/copypasta/math.py
@@ -1,5 +1,5 @@
 # 埃氏筛
-
+# https://zhuanlan.zhihu.com/p/100051075
 MX = 10**5 + 1
 omega = [0] * MX  # omega[i] 表示 i 的质因子种类数
 for i in range(2, MX):  # 预处理 omega
@@ -16,6 +16,8 @@ def eratosthenes(n):
                 is_prime[j] = False
     return [x for x in range(2, n + 1) if is_prime[x]]
 
+
+# TODO: 欧拉筛
 
 if __name__ == "__main__":
     print(eratosthenes(120))
