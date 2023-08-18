739404b777f1e8e467a76bef384758b794d7b56d
shiyang07ca <shiyang07ca@gmail.com>
2023-08-18 13:37:57 +0800

    algo common

---
+++ algo/copypasta/common.py
@@ -0,0 +1,17 @@
+# 二维前缀和
+
+
+# 二维前缀和模板
+# LC1444/周赛188D https://leetcode.cn/problems/number-of-ways-of-cutting-a-pizza/
+class MatrixSum:
+    def __init__(self, matrix: List[str]):
+        m, n = len(matrix), len(matrix[0])
+        s = [[0] * (n + 1) for _ in range(m + 1)]
+        for i, row in enumerate(matrix):
+            for j, x in enumerate(row):
+                s[i + 1][j + 1] = s[i + 1][j] + s[i][j + 1] - s[i][j] + x
+        self.s = s
+
+    # 返回左上角在 (r1,c1) 右下角在 (r2-1,c2-1) 的子矩阵元素和（类似前缀和的左闭右开）
+    def query(self, r1: int, c1: int, r2: int, c2: int) -> int:
+        return self.s[r2][c2] - self.s[r2][c1] - self.s[r1][c2] + self.s[r1][c1]
