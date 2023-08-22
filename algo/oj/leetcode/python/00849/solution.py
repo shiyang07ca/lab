10fa20774e78d8e6dedd5d8145cc92a0b9cbc2d4
shiyang07ca <shiyang07ca@gmail.com>
2023-08-22 09:54:44 +0800

    x

--- algo/oj/leetcode/python/00849/solution.py
+++ algo/oj/leetcode/python/00849/solution.py
@@ -7,6 +7,8 @@ from leetgo_py import *
 
 # @lc code=begin
 
+# tag: 前后缀分解
+
 
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
@@ -25,6 +27,26 @@ class Solution:
                 ri = i
 
         return max(min(a, b) for i, (a, b) in enumerate(zip(l, r)) if seats[i] == 0)
+
+    # 链接：https://leetcode.cn/problems/maximize-distance-to-closest-person/solutions/2399061/python3javago-qian-hou-zhui-fen-jie-by-x-1k2b/
+    def maxDistToClosest2(self, seats: List[int]) -> int:
+        n = len(seats)
+        suffix = [inf] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            x = seats[i]
+            if x == 1:
+                suffix[i] = i
+            else:
+                suffix[i] = suffix[i + 1]
+
+        j = -inf
+        res = 0
+        for i, x in enumerate(seats):
+            if x == 1:
+                j = i
+                continue
+            res = max(res, min(i - j, suffix[i] - i))
+        return res
 
 
 # @lc code=end
