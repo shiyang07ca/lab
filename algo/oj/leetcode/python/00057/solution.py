e19152182e8b24388099ddbb7a8ba60bef46e908
shiyang07ca <shiyang07ca@gmail.com>
2023-08-28 13:29:35 +0800

    x

--- algo/oj/leetcode/python/00057/solution.py
+++ algo/oj/leetcode/python/00057/solution.py
@@ -9,7 +9,7 @@ from leetgo_py import *
 
 
 class Solution:
-    def insert(
+    def insert1(
         self, intervals: List[List[int]], newInterval: List[int]
     ) -> List[List[int]]:
         intervals.append(newInterval)
@@ -20,6 +20,28 @@ class Solution:
                 ans[-1] = [ans[-1][0], max(ans[-1][1], b)]
             else:
                 ans.append([a, b])
+        return ans
+
+    # 链接：https://leetcode.cn/problems/insert-interval/solutions/2414501/python3javacgotypescript-yi-ti-shuang-ji-2fsw/
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        st, ed = newInterval
+        ans = []
+        insert = False
+        for s, e in intervals:
+            if ed < s:
+                if not insert:
+                    ans.append([st, ed])
+                    insert = True
+                ans.append([s, e])
+            elif e < st:
+                ans.append([s, e])
+            else:
+                st = min(st, s)
+                ed = max(ed, e)
+        if not insert:
+            ans.append([st, ed])
         return ans
 
 
