981d315e94277365dd727bd093aef3f8c790b4bc
shiyang07ca <shiyang07ca@gmail.com>
2023-08-29 13:59:34 +0800

    x

--- algo/oj/leetcode/python/00823/solution.py
+++ algo/oj/leetcode/python/00823/solution.py
@@ -11,7 +11,7 @@ from leetgo_py import *
 
 
 class Solution:
-    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+    def numFactoredBinaryTrees1(self, arr: List[int]) -> int:
         MOD = 10**9 + 7
         n = len(arr)
         arr.sort()
@@ -36,6 +36,23 @@ class Solution:
             return ans
 
         return sum(dfs(i) for i in range(n)) % MOD
+
+    # 链接：https://leetcode.cn/problems/binary-trees-with-factors/solutions/2416115/cong-ji-yi-hua-sou-suo-dao-di-tui-jiao-n-nbk6/
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        arr.sort()
+        idx = {x: i for i, x in enumerate(arr)}
+
+        @cache  # 缓存装饰器，避免重复计算 dfs 的结果
+        def dfs(i: int) -> int:
+            res = 1
+            val = arr[i]
+            for j in range(i):  # val 的因子一定比 val 小
+                x = arr[j]
+                if val % x == 0 and val // x in idx:  # 另一个因子 val/x 必须在 arr 中
+                    res += dfs(j) * dfs(idx[val // x])
+            return res
+
+        return sum(dfs(i) for i in range(len(arr))) % (10**9 + 7)
 
 
 # @lc code=end
