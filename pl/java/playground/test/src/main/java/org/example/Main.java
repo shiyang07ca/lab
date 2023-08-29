2a5bf0e8f4d234d81bc5a96a26fa3b9db7c46048
shiyang07ca <shiyang07ca@gmail.com>
2023-08-29 22:02:52 +0800

    x

--- pl/java/playground/test/src/main/java/org/example/Main.java
+++ pl/java/playground/test/src/main/java/org/example/Main.java
@@ -1,19 +1,62 @@
 package org.example;
 
-// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.beans.*;
+
+class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Press ⌥⏎ with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
         // Press ⇧F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
             // Press ⇧F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing ⌃F8.
             System.out.println("i = " + i);
         }
+
+        testIntrospect();
+
+    }
+
+    private static void testIntrospect() throws Exception {
+        BeanInfo info = Introspector.getBeanInfo(Person.class);
+        for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
+            System.out.println(pd.getName());
+            System.out.println("  " + pd.getReadMethod());
+            System.out.println("  " + pd.getWriteMethod());
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
