103c62cf0bbdf390b0a84c555be1e17df24b9673
shiyang07ca <shiyang07ca@gmail.com>
2023-09-04 13:33:54 +0800

    x

--- algo/oj/leetcode/python/00449/solution.py
+++ algo/oj/leetcode/python/00449/solution.py
@@ -53,6 +53,41 @@ class Codec1:
         return dfs(0, len(vs) - 1)
 
 
+# https://leetcode.cn/problems/serialize-and-deserialize-bst/solutions/2425348/python3javacgo-yi-ti-yi-jie-xian-xu-bian-7ktz/
+class Codec2:
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string."""
+
+        def dfs(root: Optional[TreeNode]):
+            if root is None:
+                return
+            nums.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+
+        nums = []
+        dfs(root)
+        return " ".join(map(str, nums))
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree."""
+
+        def dfs(mi: int, mx: int) -> Optional[TreeNode]:
+            nonlocal i
+            if i == len(nums) or not mi <= nums[i] <= mx:
+                return None
+            x = nums[i]
+            root = TreeNode(x)
+            i += 1
+            root.left = dfs(mi, x)
+            root.right = dfs(x, mx)
+            return root
+
+        nums = list(map(int, data.split()))
+        i = 0
+        return dfs(-inf, inf)
+
+
 # 链接：https://leetcode.cn/problems/serialize-and-deserialize-bst/solutions/1479903/xu-lie-hua-he-fan-xu-lie-hua-er-cha-sou-5m9r4/
 class Codec:
     def serialize(self, root: TreeNode) -> str:
@@ -83,7 +118,6 @@ class Codec:
         return construct(-inf, inf)
 
 
-# Your Codec object will be instantiated and called as such:
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
