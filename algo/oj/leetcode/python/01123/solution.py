6148633dc6aa4b20b18ba2de129ef9fdea5acdd3
shiyang07ca <shiyang07ca@gmail.com>
2023-09-06 13:32:49 +0800

    x

--- algo/oj/leetcode/python/01123/solution.py
+++ algo/oj/leetcode/python/01123/solution.py
@@ -16,20 +16,39 @@ from leetgo_py import *
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(node):
-            if not node:
-                return 0, None
 
-            dep_l, node_l = dfs(node.left)
-            dep_r, node_r = dfs(node.right)
-            if dep_l == dep_r:
-                return dep_l + 1, node
-            elif dep_l < dep_r:
-                return dep_r + 1, node_r
-            else:
-                return dep_l + 1, node_l
+
+class Solution:
+    # 链接：https://leetcode.cn/problems/lowest-common-ancestor-of-deepest-leaves/solutions/2428724/liang-chong-di-gui-si-lu-pythonjavacgojs-xxnk/
+    def lcaDeepestLeaves1(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        ans = None
+        max_depth = -1  # 全局最大深度
+
+        def dfs(node: Optional[TreeNode], depth: int) -> int:
+            nonlocal ans, max_depth
+            if node is None:
+                max_depth = max(max_depth, depth)  # 维护全局最大深度
+                return depth
+            left_max_depth = dfs(node.left, depth + 1)  # 获取左子树最深叶节点的深度
+            right_max_depth = dfs(node.right, depth + 1)  # 获取右子树最深叶节点的深度
+            if left_max_depth == right_max_depth == max_depth:
+                ans = node
+            return max(left_max_depth, right_max_depth)  # 当前子树最深叶节点的深度
+
+        dfs(root, 0)
+        return ans
+
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node: Optional[TreeNode]) -> (int, Optional[TreeNode]):
+            if node is None:
+                return 0, None
+            left_height, left_lca = dfs(node.left)
+            right_height, right_lca = dfs(node.right)
+            if left_height > right_height:  # 左子树更高
+                return left_height + 1, left_lca
+            if left_height < right_height:  # 右子树更高
+                return right_height + 1, right_lca
+            return left_height + 1, node  # 一样高
 
         return dfs(root)[1]
 
