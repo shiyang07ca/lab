f0bb6834c8425518d4f7f0f1655857b63aa269ba
shiyang07ca <shiyang07ca@gmail.com>
2023-09-16 00:06:28 +0800

    lc 198

---
+++ algo/oj/leetcode/python/00198/solution.py
@@ -0,0 +1,29 @@
+# Created by shiyang07ca at 2023/09/16 00:00
+# leetgo: dev
+# https://leetcode.cn/problems/house-robber/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dfs(i):
+            if i < 0:
+                return 0
+            return max(dfs(i - 1), dfs(i - 2) + nums[i])
+
+        n = len(nums)
+        return dfs(n - 1)
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    ans = Solution().rob(nums)
+
+    print("\noutput:", serialize(ans))
