df55681dbcde042defdb4125513b7644f350d3ac
shiyang07ca <shiyang07ca@gmail.com>
2023-09-19 22:55:34 +0800

    lc 2560 tag:TODO

--- algo/oj/leetcode/python/02560/solution.py
+++ algo/oj/leetcode/python/02560/solution.py
@@ -10,6 +10,21 @@ from leetgo_py import *
 # TODO
 # tag: binary search, dp
 
+"""
+链接：https://leetcode.cn/problems/house-robber-iv/solution/er-fen-da-an-dp-by-endlesscheng-m558/
+
+设二分的最大金额为 mx，定义 f[i] 表示在前 i 个房屋中窃取金额不超过 mx 的房屋最大个数
+
+分类讨论：
+  * 不选第 i 个房屋：f[i] = f[i-1]
+  * 选第 i 个房屋，前提是金额不超过 mx：f[i] = f[i-2] + 1
+两者取最大值，即
+       f[i] = max(f[i−1], f[i−2]+1)
+
+代码实现时，可以用两个变量滚动计算。
+
+"""
+
 
 class Solution:
     def minCapability(self, nums: List[int], k: int) -> int:
