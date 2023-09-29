b9ea63cb2b0f397da094f8decf9e9123a3985a4f
shiyang07ca <shiyang07ca@gmail.com>
2023-09-29 12:49:57 +0800

    lc 605

---
+++ algo/oj/leetcode/python/00605/solution.py
@@ -0,0 +1,36 @@
+# Created by shiyang07ca at 2023/09/29 12:06
+# leetgo: dev
+# https://leetcode.cn/problems/can-place-flowers/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        N = len(flowerbed)
+        for i, x in enumerate(flowerbed):
+            if n <= 0:
+                break
+
+            if (
+                x == 0
+                and (i > 0 and flowerbed[i - 1] == 0 or i == 0)
+                and (i < N - 1 and flowerbed[i + 1] == 0 or i == N - 1)
+            ):
+                n -= 1
+                flowerbed[i] = 1
+
+        return n <= 0
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    flowerbed: List[int] = deserialize("List[int]", read_line())
+    n: int = deserialize("int", read_line())
+    ans = Solution().canPlaceFlowers(flowerbed, n)
+
+    print("\noutput:", serialize(ans))
