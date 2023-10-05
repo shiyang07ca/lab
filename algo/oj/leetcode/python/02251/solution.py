dde43602bc9866a5e7b16387c6525fd4826dad41
shiyang07ca <shiyang07ca@gmail.com>
2023-10-05 23:44:59 +0800

    x

--- algo/oj/leetcode/python/02251/solution.py
+++ algo/oj/leetcode/python/02251/solution.py
@@ -12,12 +12,29 @@ from leetgo_py import *
 
 class Solution:
     # 链接：https://leetcode.cn/problems/number-of-flowers-in-full-bloom/description/
-    def fullBloomFlowers(
+    def fullBloomFlowers1(
         self, flowers: List[List[int]], people: List[int]
     ) -> List[int]:
         starts = sorted(s for s, _ in flowers)
         ends = sorted(e for _, e in flowers)
         return [bisect_right(starts, p) - bisect_left(ends, p) for p in people]
+
+    def fullBloomFlowers(
+        self, flowers: List[List[int]], people: List[int]
+    ) -> List[int]:
+        diff = Counter()
+        for start, end in flowers:
+            diff[start] += 1
+            diff[end + 1] -= 1
+        times = sorted(diff.keys())
+
+        j = s = 0
+        for p, i in sorted(zip(people, range(len(people)))):
+            while j < len(times) and times[j] <= p:
+                s += diff[times[j]]  # 累加不超过 people[i] 的差分值
+                j += 1
+            people[i] = s  # 从而得到这个时刻花的数量
+        return people
 
 
 # @lc code=end
