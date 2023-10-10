b33e4e0fd8121935dc7f0a3f7926983fd913b443
shiyang07ca <shiyang07ca@gmail.com>
2023-10-10 13:37:11 +0800

    lc 2731

---
+++ algo/oj/leetcode/python/02731/solution.py
@@ -0,0 +1,37 @@
+# Created by shiyang07ca at 2023/10/10 00:02
+# leetgo: dev
+# https://leetcode.cn/problems/movement-of-robots/
+
+from itertools import *
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    def sumDistance(self, nums: List[int], s: str, d: int) -> int:
+        MOD = 10**9 + 7
+        pos = []
+        for i, n in enumerate(nums):
+            if s[i] == "L":
+                pos.append(n - d)
+            else:
+                pos.append(n + d)
+        pos.sort()
+        ans = 0
+        n = len(nums)
+        for i, (a, b) in zip(range(n), pairwise(pos)):
+            ans = (ans + (i + 1) * (n - i - 1) * (b - a)) % MOD
+        return ans
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    s: str = deserialize("str", read_line())
+    d: int = deserialize("int", read_line())
+    ans = Solution().sumDistance(nums, s, d)
+
+    print("\noutput:", serialize(ans))
