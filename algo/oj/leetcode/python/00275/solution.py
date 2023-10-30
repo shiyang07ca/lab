8b8b42ba50775c2cb1d4b3273d81b0e931bd5c27
shiyang07ca <shiyang07ca@gmail.com>
2023-10-30 13:58:54 +0800

    x

--- algo/oj/leetcode/python/00275/solution.py
+++ algo/oj/leetcode/python/00275/solution.py
@@ -7,14 +7,28 @@ from leetgo_py import *
 
 # @lc code=begin
 
+# tag: binary search
+
 
 class Solution:
-    def hIndex(self, citations: List[int]) -> int:
+    def hIndex1(self, citations: List[int]) -> int:
         citations.sort(reverse=True)
         for i, c in enumerate(citations, start=1):
             if c < i:
                 return i - 1
         return i
+
+    # https://leetcode.cn/problems/h-index-ii/solutions/871112/gong-shui-san-xie-liang-chong-er-fen-ji-sovjb
+    def hIndex(self, cs: List[int]) -> int:
+        n = len(cs)
+        l, r = 0, n - 1
+        while l < r:
+            mid = (l + r) >> 1
+            if cs[mid] >= n - mid:
+                r = mid
+            else:
+                l = mid + 1
+        return n - r if cs[r] >= n - r else 0
 
 
 # @lc code=end
