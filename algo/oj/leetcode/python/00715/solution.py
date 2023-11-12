01f19df09637b507bee16927fcf5ea973f2e1681
shiyang07ca <shiyang07ca@gmail.com>
2023-11-12 23:21:27 +0800

    lc 715 tag:TODO

---
+++ algo/oj/leetcode/python/00715/solution.py
@@ -0,0 +1,129 @@
+# Created by shiyang07ca at 2023/11/12 21:59
+# leetgo: dev
+# https://leetcode.cn/problems/range-module/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+# TODO:
+
+# 链接：https://leetcode.cn/problems/range-module/description/
+
+
+class Node:
+    __slots__ = ["left", "right", "add", "v"]
+
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.add = 0
+        self.v = False
+
+
+class SegmentTree:
+    __slots__ = ["root"]
+
+    def __init__(self):
+        self.root = Node()
+
+    def modify(self, left, right, v, l=1, r=int(1e9), node=None):
+        if node is None:
+            node = self.root
+        if l >= left and r <= right:
+            if v == 1:
+                node.add = 1
+                node.v = True
+            else:
+                node.add = -1
+                node.v = False
+            return
+        self.pushdown(node)
+        mid = (l + r) >> 1
+        if left <= mid:
+            self.modify(left, right, v, l, mid, node.left)
+        if right > mid:
+            self.modify(left, right, v, mid + 1, r, node.right)
+        self.pushup(node)
+
+    def query(self, left, right, l=1, r=int(1e9), node=None):
+        if node is None:
+            node = self.root
+        if l >= left and r <= right:
+            return node.v
+        self.pushdown(node)
+        mid = (l + r) >> 1
+        v = True
+        if left <= mid:
+            v = v and self.query(left, right, l, mid, node.left)
+        if right > mid:
+            v = v and self.query(left, right, mid + 1, r, node.right)
+        return v
+
+    def pushup(self, node):
+        node.v = bool(node.left and node.left.v and node.right and node.right.v)
+
+    def pushdown(self, node):
+        if node.left is None:
+            node.left = Node()
+        if node.right is None:
+            node.right = Node()
+        if node.add:
+            node.left.add = node.right.add = node.add
+            node.left.v = node.add == 1
+            node.right.v = node.add == 1
+            node.add = 0
+
+
+class RangeModule:
+    def __init__(self):
+        self.tree = SegmentTree()
+
+    def addRange(self, left: int, right: int) -> None:
+        self.tree.modify(left, right - 1, 1)
+
+    def queryRange(self, left: int, right: int) -> bool:
+        return self.tree.query(left, right - 1)
+
+    def removeRange(self, left: int, right: int) -> None:
+        self.tree.modify(left, right - 1, -1)
+
+
+# Your RangeModule object will be instantiated and called as such:
+# obj = RangeModule()
+# obj.addRange(left,right)
+# param_2 = obj.queryRange(left,right)
+# obj.removeRange(left,right)
+
+# @lc code=end
+
+if __name__ == "__main__":
+    ops: List[str] = deserialize("List[str]", read_line())
+    params = split_array(read_line())
+    output = ["null"]
+
+    obj = RangeModule()
+
+    for i in range(1, len(ops)):
+        match ops[i]:
+            case "addRange":
+                method_params = split_array(params[i])
+                left: int = deserialize("int", method_params[0])
+                right: int = deserialize("int", method_params[1])
+                obj.addRange(left, right)
+                output.append("null")
+            case "queryRange":
+                method_params = split_array(params[i])
+                left: int = deserialize("int", method_params[0])
+                right: int = deserialize("int", method_params[1])
+                ans = serialize(obj.queryRange(left, right))
+                output.append(ans)
+            case "removeRange":
+                method_params = split_array(params[i])
+                left: int = deserialize("int", method_params[0])
+                right: int = deserialize("int", method_params[1])
+                obj.removeRange(left, right)
+                output.append("null")
+
+    print("\noutput:", join_array(output))
