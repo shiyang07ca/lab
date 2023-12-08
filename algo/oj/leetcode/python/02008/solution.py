a2be0db8dfed187e1d2a8cf46e166576db7a86a2
shiyang07ca <shiyang07ca@gmail.com>
2023-12-08 22:13:35 +0800

    x

--- algo/oj/leetcode/python/02008/solution.py
+++ algo/oj/leetcode/python/02008/solution.py
@@ -12,7 +12,7 @@ from leetgo_py import *
 
 class Solution:
     # 链接：https://leetcode.cn/problems/maximum-earnings-from-taxi/
-    def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+    def maxTaxiEarnings1(self, n: int, rides: List[List[int]]) -> int:
         groups = defaultdict(list)
         for start, end, tip in rides:
             groups[end].append((start, end - start + tip))
@@ -24,6 +24,16 @@ class Solution:
             return max(dfs(i - 1), max((dfs(s) + t for s, t in groups[i]), default=0))
 
         return dfs(n)
+
+    def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+        rides.sort(key=lambda x: x[1])
+        ends = [r[1] for r in rides]
+        f = [0] * (len(rides) + 1)
+        for i, (st, ed, p) in enumerate(rides, 1):
+            j = bisect_right(ends, st, hi=i - 1)
+            f[i] = max(f[i - 1], f[j] + ed - st + p)
+
+        return f[-1]
 
 
 # @lc code=end
