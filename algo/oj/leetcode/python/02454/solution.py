00b464a81873de294676f2b7b02631e6e502d5d6
shiyang07ca <shiyang07ca@gmail.com>
2023-12-12 23:03:47 +0800

    x

--- algo/oj/leetcode/python/02454/solution.py
+++ algo/oj/leetcode/python/02454/solution.py
@@ -7,10 +7,12 @@ from leetgo_py import *
 
 # @lc code=begin
 
+from sortedcontainers import SortedList
+
 
 class Solution:
     # 链接：https://leetcode.cn/problems/next-greater-element-iv/solutions/1935877/by-endlesscheng-q6t5/?envType=daily-question&envId=2023-12-12
-    def secondGreaterElement(self, nums: List[int]) -> List[int]:
+    def secondGreaterElement1(self, nums: List[int]) -> List[int]:
         ans = [-1] * len(nums)
         s = []
         t = []
@@ -23,6 +25,20 @@ class Solution:
             t += s[j + 1 :]  # 把从 s 弹出的这一整段元素加到 t
             del s[j + 1 :]  # 弹出一整段元素
             s.append(i)  # 当前元素（的下标）加到 s 栈顶
+        return ans
+
+    # https://leetcode.cn/problems/next-greater-element-iv/solutions/2563191/python3javactypescript-yi-ti-yi-jie-pai-sysin/?envType=daily-question&envId=2023-12-12
+    def secondGreaterElement(self, nums: List[int]) -> List[int]:
+        arr = [(x, i) for i, x in enumerate(nums)]
+        arr.sort(key=lambda x: -x[0])
+        sl = SortedList()
+        n = len(nums)
+        ans = [-1] * n
+        for _, i in arr:
+            j = sl.bisect_right(i)
+            if j + 1 < len(sl):
+                ans[i] = nums[sl[j + 1]]
+            sl.add(i)
         return ans
 
 
