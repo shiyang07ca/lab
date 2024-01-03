d128dc0420d205f78d5617bc16373dcb836cbda4
shiyang07ca <shiyang07ca@gmail.com>
2024-01-03 19:31:14 +0800

    x

---
+++ pl/py/test_dataclass.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+
+@dataclass
+class Rectangle:
+    _width: float
+    _height: float
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def area(self):
+        return self.width * self.height
+
+    @property
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+        # 在 width 发生变化时更新 area 和 perimeter
+        self._area = self.width * self.height
+        self._perimeter = 2 * (self.width + self.height)
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+        # 在 height 发生变化时更新 area 和 perimeter
+        self._area = self.width * self.height
+        self._perimeter = 2 * (self.width + self.height)
+
+
+if __name__ == '__main__':
+    # 创建一个 Rectangle 对象
+    rectangle = Rectangle(3, 4)
+
+    # 输出初始的 area 和 perimeter
+    print(rectangle.area)      # 输出: 12.0
+    print(rectangle.perimeter) # 输出: 14.0
+
+    # 修改 width 和 height 的值
+    rectangle.width = 5
+    rectangle.height = 6
+
+    # 输出更新后的 area 和 perimeter
+    print(rectangle.area)      # 输出: 30.0
+    print(rectangle.perimeter) # 输出: 22.0
