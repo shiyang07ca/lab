54330eba0c37c9739f0a0aab2a1aa871e218dd4e
shiyang07ca <shiyang07ca@gmail.com>
2024-01-11 21:37:52 +0800

    lc 2645

---
+++ algo/oj/leetcode/python/02645/solution.py
@@ -0,0 +1,42 @@
+# Created by shiyang07ca at 2024/01/11 21:30
+# leetgo: dev
+# https://leetcode.cn/problems/minimum-additions-to-make-valid-string/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        ans = i = 0
+        N = len(word)
+        while True:
+            if i >= N:
+                break
+
+            if i + 3 <= N:
+                if word[i : i + 3] == "abc":
+                    i += 3
+                    continue
+
+            if i + 2 <= N:
+                if word[i : i + 2] in ("ab", "bc", "ac"):
+                    i += 2
+                    ans += 1
+                    continue
+
+            i += 1
+            ans += 2
+
+        return ans
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    word: str = deserialize("str", read_line())
+    ans = Solution().addMinimum(word)
+
+    print("\noutput:", serialize(ans))
