62f33ec8dbbffc0c878dcbc4a9a26614db81515a
shiyang07ca <shiyang07ca@gmail.com>
2024-01-22 21:32:07 +0800

    x

--- algo/oj/leetcode/python/00670/solution.py
+++ algo/oj/leetcode/python/00670/solution.py
@@ -9,7 +9,7 @@ from leetgo_py import *
 
 
 class Solution:
-    def maximumSwap(self, num: int) -> int:
+    def maximumSwap1(self, num: int) -> int:
         nums = []
         while num:
             nums.insert(0, num % 10)
@@ -25,6 +25,21 @@ class Solution:
                         ans[j] = n
                         return int("".join(str(n) for n in ans))
         return int("".join(str(n) for n in ans))
+
+    def maximumSwap(self, num: int) -> int:
+        s = str(num)
+        max_idx = len(s) - 1
+        p = q = -1
+        for i in range(len(s) - 2, -1, -1):
+            if s[i] > s[max_idx]:  # s[i] 是目前最大数字
+                max_idx = i
+            elif s[i] < s[max_idx]:  # s[i] 右边有比它大的
+                p, q = i, max_idx  # 更新 p 和 q
+        if p == -1:  # 这意味着 s 是降序的
+            return num
+        s = list(s)
+        s[p], s[q] = s[q], s[p]  # 交换 s[p] 和 s[q]
+        return int("".join(s))
 
 
 # @lc code=end
