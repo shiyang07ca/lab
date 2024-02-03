bec02e74680abab4f09cbaa63b8ceec33b8d8038
shiyang07ca <shiyang07ca@gmail.com>
2024-02-03 15:20:05 +0800

    x

--- algo/oj/leetcode/python/01686/solution.py
+++ algo/oj/leetcode/python/01686/solution.py
@@ -12,7 +12,7 @@ from leetgo_py import *
 
 class Solution:
     # 链接：https://leetcode.cn/problems/stone-game-vi/solutions/832500/python-tan-xin-by-qubenhao-0vr4/
-    def stoneGameVI(self, aliceValues: List[int], bobValues: List[int]) -> int:
+    def stoneGameVI1(self, aliceValues: List[int], bobValues: List[int]) -> int:
         totalValues = [(a + b) for a, b in zip(aliceValues, bobValues)]
         totalValues.sort(reverse=True)
         # 所有Alice能拿到的石头的总价值，其中每个都多拿了Bob的对应石子,
@@ -23,6 +23,12 @@ class Solution:
         elif ans < 0:
             return -1
         return 0
+
+    # 链接：https://leetcode.cn/problems/stone-game-vi/solutions/2628498/xiang-xi-jie-shi-wei-shi-yao-yao-an-zhao-0zsg/
+    def stoneGameVI(self, a: List[int], b: List[int]) -> int:
+        pairs = sorted(zip(a, b), key=lambda p: -p[0] - p[1])
+        diff = sum(x if i % 2 == 0 else -y for i, (x, y) in enumerate(pairs))
+        return (diff > 0) - (diff < 0)
 
 
 # @lc code=end
