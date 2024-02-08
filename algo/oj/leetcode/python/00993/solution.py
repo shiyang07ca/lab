4ad7b135ca1fe23eeb230b4282f42b035111cf9f
shiyang07ca <shiyang07ca@gmail.com>
2024-02-08 13:10:47 +0800

    x

--- algo/oj/leetcode/python/00993/solution.py
+++ algo/oj/leetcode/python/00993/solution.py
@@ -15,7 +15,7 @@ from leetgo_py import *
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+    def isCousins1(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         queue = [root]
         while queue:
             level = []
@@ -40,6 +40,23 @@ class Solution:
                     return True
 
         return False
+
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        def dfs(node, fa, depth, t):
+            if node is None:
+                return (-1, -1)
+            if node.val == t:
+                return fa.val if fa is not None else 1, depth
+
+            l = dfs(node.left, node, depth + 1, t)
+            if l[0] != -1:
+                return l
+            print(node.val)
+            return dfs(node.right, node, depth + 1, t)
+
+        xi = dfs(root, None, 0, x)
+        yi = dfs(root, None, 0, y)
+        return xi[0] != yi[0] and xi[1] == yi[1]
 
 
 # @lc code=end
