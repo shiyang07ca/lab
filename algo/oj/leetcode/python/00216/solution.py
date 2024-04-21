ef159204e7c14ce1f9e6e0cce6ced6f1710ce7a4
shiyang07ca <shiyang07ca@gmail.com>
2024-04-21 13:47:34 +0800

    x

--- algo/oj/leetcode/python/00216/solution.py
+++ algo/oj/leetcode/python/00216/solution.py
@@ -9,7 +9,7 @@ from leetgo_py import *
 
 
 class Solution:
-    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+    def combinationSum31(self, k: int, n: int) -> List[List[int]]:
         ans = []
         path = []
 
@@ -28,6 +28,26 @@ class Solution:
             dfs(i + 1, left)
 
         dfs(1, n)
+        return ans
+
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+        path = []
+
+        def dfs(i, left):
+            if left == 0 and len(path) == k:
+                ans.append(path[:])
+                return
+
+            if len(path) > k or left < i:
+                return
+
+            for j in range(i + 1, 10):
+                path.append(j)
+                dfs(j, left - j)
+                path.pop()
+
+        dfs(0, n)
         return ans
 
 
