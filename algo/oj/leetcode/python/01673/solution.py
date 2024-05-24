7fd744ba7da2d49c79376e48e2fad2104c0fb43d
shiyang07ca <shiyang07ca@gmail.com>
2024-05-24 23:41:10 +0800

    lc 1673 tag:TODO

---
+++ algo/oj/leetcode/python/01673/solution.py
@@ -0,0 +1,29 @@
+# Created by shiyang07ca at 2024/05/24 22:18
+# leetgo: dev
+# https://leetcode.cn/problems/find-the-most-competitive-subsequence/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    # 链接：https://leetcode.cn/problems/find-the-most-competitive-subsequence/solutions/2789156/letmefly-1673zhao-chu-zui-ju-jing-zheng-dihu5/
+    def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
+        st = []
+        for i in range(len(nums)):
+            while st and st[-1] > nums[i] and (len(st) - 1) + (len(nums) - i) >= k:
+                st.pop()
+            if len(st) < k:
+                st.append(nums[i])
+        return st
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    k: int = deserialize("int", read_line())
+    ans = Solution().mostCompetitive(nums, k)
+    print("\noutput:", serialize(ans, "integer[]"))
