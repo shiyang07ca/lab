2ce47d94a22d3c5e2cdee2b975bf93490bb5ab66
shiyang07ca <shiyang07ca@gmail.com>
2024-05-29 23:59:03 +0800

    lc 2981

---
+++ algo/oj/leetcode/python/02981/solution.py
@@ -0,0 +1,36 @@
+# Created by shiyang07ca at 2024/05/29 23:57
+# leetgo: dev
+# https://leetcode.cn/problems/find-longest-special-substring-that-occurs-thrice-i/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    # 链接：https://leetcode.cn/problems/find-longest-special-substring-that-occurs-thrice-i/solutions/
+    def maximumLength(self, s: str) -> int:
+        groups = defaultdict(list)
+        cnt = 0
+        for i, ch in enumerate(s):
+            cnt += 1
+            if i + 1 == len(s) or ch != s[i + 1]:
+                groups[ch].append(cnt)  # 统计连续字符长度
+                cnt = 0
+
+        ans = 0
+        for a in groups.values():
+            a.sort(reverse=True)
+            a.extend([0, 0])  # 假设还有两个空串
+            ans = max(ans, a[0] - 2, min(a[0] - 1, a[1]), a[2])
+
+        return ans if ans else -1
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    s: str = deserialize("str", read_line())
+    ans = Solution().maximumLength(s)
+    print("\noutput:", serialize(ans, "integer"))
