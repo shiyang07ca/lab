7ba30dc19dc5334603b136904f0938838d63519c
shiyang07ca <shiyang07ca@gmail.com>
2024-10-17 13:58:21 +0800

    Py type

---
+++ pl/py/src/py/type/generic.py
@@ -0,0 +1,28 @@
+from typing import Generic, TypeVar
+
+# Before 3.12 you have to write:
+# T = TypeVar("T")
+
+
+# class Box(Generic[T]):
+#     def __init__(self, content: T):
+#         self.content = content
+
+#     def get_content(self) -> T:
+#         return self.content
+
+
+# For Python >= 3.12
+class Box[T]:
+    def __init__(self, content: T):
+        self.content = content
+
+    def get_content(self) -> T:
+        return self.content
+
+
+int_box = Box(123)
+print(int_box.get_content())  # 输出：123
+
+str_box = Box("hello")
+print(str_box.get_content())  # 输出：hello
