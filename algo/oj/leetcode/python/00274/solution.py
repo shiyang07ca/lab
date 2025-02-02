ae6224bb4764fc90d5ce02fc15deb672bc775b9a
shiyang07ca <shiyang07ca@gmail.com>
2025-02-02 14:10:40 +0800

    x

--- algo/oj/leetcode/python/00274/solution.py
+++ algo/oj/leetcode/python/00274/solution.py
@@ -3,18 +3,31 @@
 # https://leetcode.cn/problems/h-index/
 
 from typing import *
+
 from leetgo_py import *
 
 # @lc code=begin
 
 
 class Solution:
-    def hIndex(self, citations: List[int]) -> int:
+    def hIndex1(self, citations: List[int]) -> int:
         citations.sort(reverse=True)
         for i, c in enumerate(citations, start=1):
             if c < i:
                 return i - 1
         return len(citations)
+
+    # 链接：https://leetcode.cn/problems/h-index/solutions/2502896/gong-shui-san-xie-cong-po-ti-dao-zhu-bu-7sug6/
+    def hIndex(self, cs: List[int]) -> int:
+        n = len(cs)
+        l, r = 0, n
+        while l < r:
+            mid = (l + r + 1) // 2
+            if sum(c >= mid for c in cs) >= mid:
+                l = mid
+            else:
+                r = mid - 1
+        return r
 
 
 # @lc code=end
--- algo/reverse_stack.py
+++ algo/reverse_stack.py
@@ -6,8 +6,6 @@
 """
 
 import unittest
-
-
 from typing import List
 
 
